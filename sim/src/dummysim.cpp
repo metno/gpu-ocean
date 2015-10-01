metno/gpu-ocean

commit a5ace475e1f017501b01b6930604a17f0a9abaa6

Made sim interface abstract

sim/src/dummysim.cpp
@@ -1,32 +1,32 @@
-#include "simulator.h"
-#include <boost/format.hpp>
+#include "dummysim.h"
+#include "boost/format.hpp"
 
 using namespace std;
 
-struct Simulator::SimulatorImpl
+struct DummySim::DummySimImpl
 {
     int nextStep;
     int finalStep;
-    SimulatorImpl();
+    DummySimImpl();
 };
 
-Simulator::SimulatorImpl::SimulatorImpl()
+DummySim::DummySimImpl::DummySimImpl()
     : nextStep(-1)
     , finalStep(-1)
 {
 }
 
-Simulator::Simulator(const OptionsPtr &options, const InitCondPtr &initCond)
+DummySim::DummySim(const OptionsPtr &options, const InitCondPtr &initCond)
     : SimBase(options, initCond)
-    , pimpl(new SimulatorImpl)
+    , pimpl(new DummySimImpl)
 {
 }
 
-Simulator::~Simulator()
+DummySim::~DummySim()
 {
 }
 
-void Simulator::init()
+void DummySim::init()
 {
     pimpl->nextStep = 0;
 
@@ -34,17 +34,17 @@ void Simulator::init()
     pimpl->finalStep = 4; // ### for now
 }
 
-int Simulator::nextStep() const
+int DummySim::nextStep() const
 {
     return pimpl->nextStep;
 }
 
-int Simulator::finalStep() const
+int DummySim::finalStep() const
 {
     return pimpl->finalStep;
 }
 
-void Simulator::execNextStep()
+void DummySim::execNextStep()
 {
     if (pimpl->nextStep > pimpl->finalStep)
         throw runtime_error((boost::format("error: next_step_ (%1%) > final_step_ (%2%)") % pimpl->nextStep % pimpl->finalStep).str());
@@ -54,12 +54,12 @@ void Simulator::execNextStep()
     pimpl->nextStep++;
 }
 
-vector<float> Simulator::results() const
+vector<float> DummySim::results() const
 {
     return vector<float>(); // ### for now
 }
 
-void Simulator::printStatus() const
+void DummySim::printStatus() const
 {
-    cout << "Simulator::printStatus(); options: " << options() << endl;
+    cout << "DummySim::printStatus(); options: " << options() << endl;
 }

apps/src/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
     cout << "done\n";
 
     // get pointer to simulator object
-    SimPtr sim = mgr.sim();
+    SimBasePtr sim = mgr.sim();
     sim->printStatus();
 
     // do some OpenCL stuff

mgr/src/manager.cpp
@@ -1,21 +1,30 @@
 #include "manager.h"
 #include "programoptions.h"
 #include "initconditions.h"
+#include "simbase.h"
 #include "simulator.h"
+#include "dummysim.h"
 #include <stdexcept>
 #include <vector>
 #include <iostream>
+
+///XXX
+#define DUMMYSIM
 
 using namespace std;
 
 struct Manager::ManagerImpl
 {
-    SimPtr sim;
+    SimBasePtr sim;
     ManagerImpl(const OptionsPtr &, const InitCondPtr &);
 };
 
 Manager::ManagerImpl::ManagerImpl(const OptionsPtr &options, const InitCondPtr &initCond)
+#ifdef DUMMYSIM
+    : sim(new DummySim(options, initCond))
+#else
     : sim(new Simulator(options, initCond))
+#endif
 {
 }
 
@@ -60,7 +69,7 @@ OptionsPtr Manager::options() const
 }
 
 // Returns the simulator object.
-SimPtr Manager::sim() const
+SimBasePtr Manager::sim() const
 {
     return pimpl->sim;
 }
@@ -86,14 +95,13 @@ int Manager::finalStep() const
 // Advances the simulation one time step.
 void Manager::execNextStep()
 {
-    // process next simulation step ... TBD
     pimpl->sim->execNextStep();
 }
 
 // Returns simulation results at the current step.
 vector<float> Manager::results() const
 {
-    return vector<float>();
+    return pimpl->sim->results();
 }
 
 Manager::Manager()

mgr/src/manager.h
@@ -16,7 +16,7 @@ public:
     static bool initialized();
     static Manager &instance();
     OptionsPtr options() const;
-    SimPtr sim() const;
+    SimBasePtr sim() const;
     void initSim();
     int nextStep() const;
     int finalStep() const;

sim/CMakeLists.txt
@@ -30,7 +30,7 @@ IF(sim_SIMULATOR_VERSION STREQUAL "")
     MESSAGE(FATAL_ERROR "sim_SIMULATOR_VERSION not defined")
 ENDIF()
 
-ADD_LIBRARY(sim STATIC src/simulator.cpp)
+ADD_LIBRARY(sim STATIC src/simbase.cpp src/simulator.cpp src/dummysim.cpp)
 # MLS: Do need this next line?
 TARGET_LINK_LIBRARIES(sim ${OPENCL_LIBRARIES})
 SET_TARGET_PROPERTIES(sim PROPERTIES DEBUG_POSTFIX "d")

sim/src/dummysim.h
@@ -1,0 +1,27 @@
+#ifndef DUMMYSM_H
+#define DUMMYSIM_H
+
+#include "simbase.h"
+
+/**
+ * @brief This class tests execution of an OpenCL kernel that does nothing useful.
+ */
+class DummySim : public SimBase
+{
+public:
+    DummySim(const OptionsPtr &, const InitCondPtr &);
+    virtual ~DummySim();
+
+private:
+    virtual void init();
+    virtual int nextStep() const;
+    virtual int finalStep() const;
+    virtual void execNextStep();
+    virtual std::vector<float> results() const;
+    virtual void printStatus() const;
+
+    struct DummySimImpl;
+    DummySimImpl *pimpl;
+};
+
+#endif // DUMMYSIM_H

sim/src/simbase.cpp
@@ -1,0 +1,44 @@
+#include "simbase.h"
+
+#include "programoptions.h"
+#include "initconditions.h"
+#include <iostream>
+
+using namespace std;
+
+struct SimBase::SimBaseImpl
+{
+    OptionsPtr options;
+    InitCondPtr initCond;
+    SimBaseImpl(const OptionsPtr &, const InitCondPtr &);
+};
+
+SimBase::SimBaseImpl::SimBaseImpl(const OptionsPtr &options, const InitCondPtr &initCond)
+    : options(options)
+    , initCond(initCond)
+{
+}
+
+/**
+ * Constructor.
+ * @param options Input: Options.
+ * @param initCond Input: Initial conditions.
+ */
+SimBase::SimBase(const OptionsPtr &options, const InitCondPtr &initCond)
+    : pimpl(new SimBaseImpl(options, initCond))
+{
+}
+
+SimBase::~SimBase()
+{
+}
+
+OptionsPtr SimBase::options() const
+{
+    return pimpl->options;
+}
+
+InitCondPtr SimBase::initCond() const
+{
+    return pimpl->initCond;
+}

sim/src/simbase.h
@@ -1,0 +1,67 @@
+#ifndef SIMBASE_H
+#define SIMBASE_H
+
+#include "programoptions.h"
+#include "initconditions.h"
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+/**
+ * @brief This is the base class for managing low-level aspects of a simulation.
+ */
+class SimBase
+{
+public:
+
+    /**
+     * Initializes the simulator. This includes resetting the current and final step values.
+     */
+    virtual void init() = 0;
+
+    /**
+     * Returns the current step value.
+     */
+    virtual int nextStep() const = 0;
+
+    /**
+     * Returns the final step value.
+     */
+    virtual int finalStep() const = 0;
+
+    /**
+     * Executes the next simulation step and increases the step value by one.
+     * Throws std::runtime_error if the current step value exceeds the final one.
+     */
+    virtual void execNextStep() = 0;
+
+    /**
+     * Returns the simulation results at the current step.
+     */
+    virtual std::vector<float> results() const = 0;
+
+    virtual void printStatus() const = 0;
+
+    /**
+     * Returns the options.
+     */
+    OptionsPtr options() const;
+
+    /**
+     * Returns the initial conditions.
+     */
+    InitCondPtr initCond() const;
+
+protected:
+
+    SimBase(const OptionsPtr &, const InitCondPtr &);
+    virtual ~SimBase();
+
+private:
+
+    struct SimBaseImpl;
+    SimBaseImpl *pimpl;
+};
+
+typedef boost::shared_ptr<SimBase> SimBasePtr;
+
+#endif // SIMBASE_H

commit 2e225e1842cd2f6bd9b8651e3c1f59d2130ad009

Fixed regression
Introduced by a5ace475e1f017501b01b6930604a17f0a9abaa6

sim/src/dummysim.cpp
@@ -61,5 +61,5 @@ vector<float> DummySim::results() const
 
 void DummySim::printStatus() const
 {
-    cout << "DummySim::printStatus(); options: " << options() << endl;
+    cout << "DummySim::printStatus(); options: " << *options() << endl;
 }

sim/src/simulator.cpp
@@ -61,5 +61,5 @@ vector<float> Simulator::results() const
 
 void Simulator::printStatus() const
 {
-    cout << "Simulator::printStatus(); options: " << options() << endl;
+    cout << "Simulator::printStatus(); options: " << *options() << endl;
 }