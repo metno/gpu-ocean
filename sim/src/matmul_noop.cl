ba58890d2ec2b566842f5b610d62d089b80d09a0
Jo Asplin <jo.asplin@met.no>
2015-10-12 15:30:01 +0200

    Simplified code in attempt to debug kernel compilation problem
    Currently the dummy sim program fails in an unstable way when compiling the kernel.
    When compiling and executing both the 'full' and 'noop' kernels (i.e. defining
    both EXECFULL and EXECNOOP), the program fails with different error codes, and sometimes it
    doesn't fail at all. This happens both on the CPU and the GPU device, although on
    the GPU device the program tends to always fail.
    
    The program does not fail when only one kernel is executed (i.e. defining only one
    of EXECFULL and EXECNOOP).

--- sim/src/matmul_noop.cl
+++ sim/src/matmul_noop.cl
@@ -1,4 +1,4 @@
-__kernel void MatMul (
+__kernel void MatMulNoop (
     __global const float *a,
     __global const float *b,
     __global float *ab)
--- sim/src/matmulcl.cpp
+++ sim/src/matmulcl.cpp
@@ -10,259 +10,14 @@
 #undef NDEBUG
 #define NDEBUG
 
+#define EXECFULL
+#define EXECNOOP
+
 using namespace std;
 
 static void errorCallback(const char *errInfo, const void *privateInfo, size_t cb, void *userData)
 {
     cout << "errorCallback:" << errInfo << endl;
-}
-
-class MatMulExecutor
-{
-public:
-    MatMulExecutor(size_t, const vector<float> &ha, const vector<float> &hb, cl_device_type, const string &);
-    virtual ~MatMulExecutor() {}
-    void initialize();
-    void execute();
-    float elapsedMilliseconds() const;
-    string platformName() const;
-    string deviceName() const;
-
-private:
-    cl_int createContext(vector<cl::Device> *);
-    cl_int createInputBuffers();
-
-    struct MatMulExecutorImpl;
-    MatMulExecutorImpl *pimpl;
-};
-
-struct MatMulExecutor::MatMulExecutorImpl
-{
-    size_t size;
-    cl_device_type deviceType;
-    string kernelFileName;
-    bool initialized;
-    cl_ulong cmdStart;
-    cl_ulong cmdEnd;
-    string platformName;
-    string deviceName;
-
-    vector<float> ha;
-    vector<float> hb;
-
-    cl::Context context;
-    cl::Program program;
-    cl::Kernel kernel;
-    cl::Buffer a;
-    cl::Buffer b;
-    cl::Buffer ab;
-    cl::CommandQueue queue;
-    cl::Event event;
-
-    MatMulExecutorImpl(size_t, const vector<float> &, const vector<float> &, cl_device_type, const string &);
-};
-
-MatMulExecutor::MatMulExecutorImpl::MatMulExecutorImpl(
-        size_t size, const vector<float> &ha, const vector<float> &hb, cl_device_type deviceType, const string &kernelFileName)
-    : size(size)
-    , ha(ha)
-    , hb(hb)
-    , deviceType(deviceType)
-    , kernelFileName(kernelFileName)
-    , initialized(false)
-    , cmdStart(0)
-    , cmdEnd(0)
-{
-}
-
-MatMulExecutor::MatMulExecutor(
-        size_t size, const vector<float> &ha, const vector<float> &hb, cl_device_type deviceType, const string &kernelFileName)
-    : pimpl(new MatMulExecutorImpl(size, ha, hb, deviceType, kernelFileName))
-{
-}
-
-/**
- * Initializes the executor. Throws std::runtime_error if the matrix size is null.
- */
-void MatMulExecutor::initialize()
-{
-    if (pimpl->size <= 0)
-        throw runtime_error((boost::format("size (%1%) <= 0") % pimpl->size).str());
-
-    cl_int error;
-    vector<cl::Device> devices;
-
-    // create context
-    CL_CHECK(createContext(&devices));
-
-    // create random input matrices
-    CL_CHECK(createInputBuffers());
-
-    // create program from source
-    pimpl->program = cl::Program(pimpl->context, OpenCLUtils::loadSingleKernel(pimpl->kernelFileName.c_str()), &error);
-    CL_CHECK(error);
-
-    // compile program
-    error = pimpl->program.build(devices, (boost::format("-D MATRIX_SIZE=%d") % pimpl->size).str().c_str(), 0, 0);
-    if (error == CL_BUILD_PROGRAM_FAILURE) {
-        string log;
-        log.resize(2048);
-        cerr << "build program failure detected:\n";
-        for (int i = 0; i < devices.size(); ++i) {
-            cerr << "============ build log for device " << i << ": ============\n";
-            cl_int error2;
-            const string log = pimpl->program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[i], &error2);
-            CL_CHECK(error2);
-            cerr << log << endl;
-        }
-    }
-    CL_CHECK(error);
-
-    // create kernel
-    pimpl->kernel = cl::Kernel(pimpl->program, "MatMul", &error);
-    CL_CHECK(error);
-
-    // create buffer for output matrix
-    pimpl->ab = cl::Buffer(pimpl->context, CL_MEM_WRITE_ONLY, sizeof(float) * pimpl->size * pimpl->size, 0, &error);
-    CL_CHECK(error);
-
-    pimpl->kernel.setArg<cl::Buffer>(0, pimpl->a);
-    pimpl->kernel.setArg<cl::Buffer>(1, pimpl->b);
-    pimpl->kernel.setArg<cl::Buffer>(2, pimpl->ab);
-
-    // create command queue
-    pimpl->queue = cl::CommandQueue(pimpl->context, devices[0], CL_QUEUE_PROFILING_ENABLE, &error); // for now, use first device
-    CL_CHECK(error);
-
-    pimpl->initialized = true;
-}
-
-/**
- * Executes the OpenCL kernel.
- */
-void MatMulExecutor::execute()
-{
-    CL_CHECK(pimpl->queue.enqueueNDRangeKernel(
-                 pimpl->kernel, cl::NullRange, cl::NDRange(pimpl->size, pimpl->size), cl::NullRange, 0, &pimpl->event));
-
-    CL_CHECK(pimpl->event.wait());
-
-    pimpl->cmdStart = pimpl->event.getProfilingInfo<CL_PROFILING_COMMAND_START>();
-    pimpl->cmdEnd = pimpl->event.getProfilingInfo<CL_PROFILING_COMMAND_END>();
-
-    // copy result back to host ... TBD
-}
-
-/**
- * Returns the execution time.
- * @return Elapsed execution time in milliseconds
- */
-float MatMulExecutor::elapsedMilliseconds() const
-{
-    return (pimpl->cmdEnd - pimpl->cmdStart) * .000001; // note: cmdEnd and cmdStart are both in nanoseconds
-}
-
-/**
- * Returns the platform on which the kernel will be executed.
- * @return Platform name
- */
-string MatMulExecutor::platformName() const
-{
-    return pimpl->platformName;
-}
-
-/**
- * Returns the device on which the kernel will be executed.
- * @return Device name
- */
-string MatMulExecutor::deviceName() const
-{
-    return pimpl->deviceName;
-}
-
-/**
- * Creates the OpenCL context. Throws std::runtime_error if no relevant device is found.
- * @param devices Output: Device objects matching this->pimpl->deviceType
- * @return OpenCL error code
- */
-cl_int MatMulExecutor::createContext(vector<cl::Device> *devices)
-{
-    vector<cl::Platform> platforms;
-    OpenCLUtils::getPlatforms(&platforms);
-
-    if (platforms.empty()) {
-        throw runtime_error("No OpenCL platform found");
-    } else {
-#ifndef NDEBUG
-        cout << "Found " << platforms.size() << " platform(s)" << endl;
-#endif
-    }
-
-#ifndef NDEBUG
-    for (cl_uint i = 0; i < platforms.size(); ++i) {
-        cout << "\t (" << (i + 1) << ") : " << OpenCLUtils::getPlatformName(platforms[i]) << endl;
-    }
-#endif
-
-    cl_uint pfIndex = 0;
-
-    // loop over platforms
-    for (pfIndex = 0; pfIndex < platforms.size(); ++pfIndex) {
-        platforms[pfIndex].getDevices(pimpl->deviceType, devices);
-        if (!devices->empty())
-            break; // found at last one relevant device on this platform
-    }
-    if (pfIndex == platforms.size()) {
-        throw runtime_error("No relevant OpenCL devices found on any platform");
-    } else {
-#ifndef NDEBUG
-        cout << "Found " << devices->size() << " device(s)" << endl;
-#endif
-    }
-
-    pimpl->platformName = OpenCLUtils::getPlatformName(platforms[pfIndex]);
-
-#ifndef NDEBUG
-    for (cl_uint i = 0; i < devices.size(); ++i) {
-        cout << "\t (" << (i + 1) << ") : " << OpenCLUtils::getDeviceName(devices[i]) << endl;
-    }
-#endif
-
-    pimpl->deviceName = OpenCLUtils::getDeviceName(devices->front()); // for now, use first device
-
-    cl_context_properties contextProperties[] =
-    {
-        CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[pfIndex])(),
-        0
-    };
-    cl_int error = CL_SUCCESS;
-
-    pimpl->context = cl::Context(*devices, contextProperties, errorCallback, 0, &error);
-
-    return error;
-}
-
-/**
- * Creates OpenCL buffer objects, one for each of the two input matrices A and B.
- * @return OpenCL error code
- */
-cl_int MatMulExecutor::createInputBuffers()
-{
-    cl_int error = CL_SUCCESS;
-
-    pimpl->a = cl::Buffer(
-                pimpl->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * pimpl->size * pimpl->size,
-                pimpl->ha.data(), &error);
-    if (error != CL_SUCCESS)
-        return error;
-
-    pimpl->b = cl::Buffer(
-                pimpl->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * pimpl->size * pimpl->size,
-                pimpl->hb.data(), &error);
-    if (error != CL_SUCCESS)
-        return error;
-
-    return CL_SUCCESS;
 }
 
 /**
@@ -285,54 +40,186 @@ static void createInputMatrices(size_t size, vector<float> &a, vector<float> &b)
 }
 
 /**
- * Multiplies two NxN matrices A and B.
- * @param size Input: The size of N
- * @param a Input: The A matrix
- * @param b Input: The B matrix
- * @param deviceType Input: The device type: CL_DEVICE_TYPE_CPU or CL_DEVICE_TYPE_GPU
- * @param kernelFileName Input: The name of the kernel file (note: the value of the KERNELDIR environment variable will be prepended)
- * @param msecs Output: The elapsed execution time in milliseconds
- * @param platformName Output: If non-null, the name of the platform used for the execution
- * @param deviceName Output: If non-null, the name of the device used for the execution
+ * Returns the execution time from an event.
+ * @param event Input: The event object
+ * @return Elapsed execution time in milliseconds
  */
-static void matmulExec(
-        size_t size, const vector<float> &a, const vector<float> &b, cl_device_type deviceType, const string &kernelFileName,
-        float &msecs, string *platformName = 0, string *deviceName = 0)
+static float elapsedMilliseconds(const cl::Event &event)
 {
-    MatMulExecutor executor(size, a, b, deviceType, kernelFileName);
-    executor.initialize();
-    executor.execute();
-    msecs = executor.elapsedMilliseconds();
-    if (platformName)
-        *platformName = executor.platformName();
-    if (deviceName)
-        *deviceName = executor.deviceName();
+    return (event.getProfilingInfo<CL_PROFILING_COMMAND_END>()
+            - event.getProfilingInfo<CL_PROFILING_COMMAND_START>()) * .000001; // note: _START and _END values are both in nanoseconds
 }
 
 /**
- * Public function for multiplying two NxN matrices. Throws std::runtime_error if something goes wrong.
- * @param size: The size of N
+ * Prints the build log of a program on stderr.
+ * @param program Input: The program object
+ * @param devices Input: The device objects
+ */
+static void printProgramBuildLog(const cl::Program &program, vector<cl::Device> &devices)
+{
+    string log;
+    log.resize(2048);
+    cerr << "build program failure detected:\n";
+    for (int i = 0; i < devices.size(); ++i) {
+        cerr << "============ build log for device " << i << ": ============\n";
+        cl_int error;
+        const string log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[i], &error);
+        CL_CHECK(error);
+        cerr << log << endl;
+    }
+}
+
+/**
+ * Public function for multiplying two NxN matrices.
+ * Throws std::runtime_error if something goes wrong.
+ * @param size Input: The size of N
  * @param execOnCpu Input: Whether to execute the kernel on the CPU
  */
 void matmul(size_t size, bool execOnCpu)
 {
-    // generate random input
-    vector<float> a;
-    vector<float> b;
-    createInputMatrices(size, a, b);
+    if (size <= 0)
+        throw runtime_error((boost::format("size (%1%) <= 0") % size).str());
 
+    cl_int error = CL_SUCCESS;
+
+    // --- BEGIN get device and create context ---------------------------
+    // get platforms
+    vector<cl::Platform> platforms;
+    OpenCLUtils::getPlatforms(&platforms);
+    if (platforms.empty())
+        throw runtime_error("No OpenCL platform found");
+
+    // get device
+    vector<cl::Device> devices;
     const cl_device_type deviceType = execOnCpu ? CL_DEVICE_TYPE_CPU : CL_DEVICE_TYPE_GPU;
+    cl_uint pfmIndex = 0;
+    for (pfmIndex = 0; pfmIndex < platforms.size(); ++pfmIndex) {
+        platforms[pfmIndex].getDevices(deviceType, &devices);
+        if (!devices.empty())
+            break; // found at last one relevant device on this platform
+    }
+    if (pfmIndex == platforms.size())
+        throw runtime_error("No relevant OpenCL devices found on any platform");
 
-    string platformName;
-    string deviceName;
+    const int devIndex = 0; // for now, use first device
+    const string deviceName = OpenCLUtils::getDeviceName(devices[devIndex]);
+    const string platformName = OpenCLUtils::getPlatformName(platforms[pfmIndex]);
 
+    // create context
+    cl_context_properties contextProperties[] =
+    {
+        CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[pfmIndex])(),
+        0
+    };
+    cl::Context context(devices, contextProperties, errorCallback, 0, &error);
+    CL_CHECK(error);
+    // --- END get device and create context ---------------------------
+
+
+    // --- BEGIN set up input structures ---------------------------
+
+    // generate matrices with random values
+    vector<float> ha;
+    vector<float> hb;
+    createInputMatrices(size, ha, hb);
+
+    // create buffers
+    cl::Buffer a = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * size * size, ha.data(), &error);
+    CL_CHECK(error);
+    cl::Buffer b = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * size * size, hb.data(), &error);
+    CL_CHECK(error);
+
+    // --- END set up input structures ---------------------------
+
+
+    // --- BEGIN set up kernels and output structures ---------------------------
+
+#ifdef EXECFULL
+    // create program 1 (full multiplication) from source
+    cl::Program program_full(context, OpenCLUtils::loadSingleKernel("matmul.cl"), &error);
+    CL_CHECK(error);
+
+    // compile program 1
+    error = program_full.build(devices, (boost::format("-D MATRIX_SIZE=%d") % size).str().c_str(), 0, 0);
+    if (error == CL_BUILD_PROGRAM_FAILURE)
+        printProgramBuildLog(program_full, devices);
+    CL_CHECK(error);
+
+    // create kernel 1
+    cl::Kernel kernel_full(program_full, "MatMul", &error);
+    CL_CHECK(error);
+
+    // create buffer for output matrix
+    cl::Buffer ab_full(context, CL_MEM_WRITE_ONLY, sizeof(float) * size * size, 0, &error);
+    CL_CHECK(error);
+
+    // set kernel args
+    kernel_full.setArg<cl::Buffer>(0, a);
+    kernel_full.setArg<cl::Buffer>(1, b);
+    kernel_full.setArg<cl::Buffer>(2, ab_full);
+#endif
+
+    // -------------------------------------------------------------------------------
+
+#ifdef EXECNOOP
+    // create program 2 (only copying memory between host and device) from source
+    cl::Program program_noop(context, OpenCLUtils::loadSingleKernel("matmul_noop.cl"), &error);
+    CL_CHECK(error);
+
+    // compile program 2
+    error = program_noop.build(devices, (boost::format("-D MATRIX_SIZE=%d") % size).str().c_str(), 0, 0);
+    if (error == CL_BUILD_PROGRAM_FAILURE)
+        printProgramBuildLog(program_noop, devices);
+    CL_CHECK(error);
+
+    // create kernel 2
+    cl::Kernel kernel_noop(program_noop, "MatMulNoop", &error);
+    CL_CHECK(error);
+
+    // create buffer for output matrix
+    cl::Buffer ab_noop(context, CL_MEM_WRITE_ONLY, sizeof(float) * size * size, 0, &error);
+    CL_CHECK(error);
+
+    // set kernel args
+    kernel_noop.setArg<cl::Buffer>(0, a);
+    kernel_noop.setArg<cl::Buffer>(1, b);
+    kernel_noop.setArg<cl::Buffer>(2, ab_noop);
+#endif
+
+    // --- END set up kernels and output structures ---------------------------
+
+
+    // --- BEGIN create command queue ---------------------------
+
+    cl::CommandQueue queue(context, devices[devIndex], CL_QUEUE_PROFILING_ENABLE, &error);
+    CL_CHECK(error);
+
+    // --- END create command queue ---------------------------
+
+
+    // --- BEGIN execute kernels ---------------------------
+    cl::Event event;
+
+#ifdef EXECFULL
     // execute full multiplication
-    float msecs_full = -1.0;
-    matmulExec(size, a, b, deviceType, "matmul.cl", msecs_full, &platformName, &deviceName);
+    CL_CHECK(queue.enqueueNDRangeKernel(kernel_full, cl::NullRange, cl::NDRange(size, size), cl::NullRange, 0, &event));
+    CL_CHECK(event.wait());
+    // examine contents of ab_full ... TBD
+    const float msecs_full = elapsedMilliseconds(event);
+#else
+    const float msecs_full = -1;
+#endif
 
+#ifdef EXECNOOP
     // execute noop (only copying memory between host and device)
-    float msecs_noop = -1.0;
-    matmulExec(size, a, b, deviceType, "matmul_noop.cl", msecs_noop);
+    CL_CHECK(queue.enqueueNDRangeKernel(kernel_noop, cl::NullRange, cl::NDRange(size, size), cl::NullRange, 0, &event));
+    CL_CHECK(event.wait());
+    // examine contents of ab_noop ... TBD
+    const float msecs_noop = elapsedMilliseconds(event);
+#else
+    const float msecs_noop = -1;
+#endif
+    // --- END execute kernels ---------------------------
 
 #ifndef NDEBUG
     cout << (boost::format("matrix size: %4d x %4d;   msecs: %12.6f;   noop msecs: %12.6f   (platform: %s; device: %s)")
