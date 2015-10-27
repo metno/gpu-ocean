77a5462350cdbf4f8ff45847d2a64892901a03fc
Jo Asplin <jo.asplin@met.no>
2015-10-27 14:48:13 +0100

    Partial, host-side implementation of reconstruction of H

--- sim/src/simulator.cpp
+++ sim/src/simulator.cpp
@@ -1,4 +1,5 @@
 #include "simulator.h"
+#include "oclutils.h"
 #include <boost/format.hpp>
 #include <iostream>
 
@@ -6,12 +7,45 @@ using namespace std;
 
 struct Simulator::SimulatorImpl
 {
-    // ### nothing here yet
+    vector<float> Hr_u; // H reconstructed in x-dimension
+    vector<float> Hr_v; // H reconstructed in y-dimension
+
     SimulatorImpl();
+    void reconstructH(const InitCondPtr &);
 };
 
 Simulator::SimulatorImpl::SimulatorImpl()
 {
+}
+
+/**
+ * Reconstructs H, i.e. computes Hr_u and Hr_v from initCond->H().
+ */
+void Simulator::SimulatorImpl::reconstructH(const InitCondPtr &initCond)
+{
+    cerr << "reconstructing H ...\n";
+
+    cl_int error = CL_SUCCESS;
+
+    // create buffers for input field
+    cl::Buffer H = cl::Buffer(
+                *OpenCLUtils::getContext(), CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                sizeof(float) * initCond->H().data->size(), initCond->H().data->data(), &error);
+    CL_CHECK(error);
+
+    // set up kernel arguments ...
+    // ... in:
+    OpenCLUtils::getKernel("MatMulNoop")->setArg<cl::Buffer>(0, H);
+    // ... out:
+    // pointers to Hr_u and Hr_v in device memory ... TBD
+
+    // execute kernel (computes Hr_u and Hr_v in device memory and returns pointers)
+    cl::Event event;
+    CL_CHECK(OpenCLUtils::getQueue()->enqueueNDRangeKernel(
+                 *OpenCLUtils::getKernel("ReconstructH"), cl::NullRange, cl::NDRange(initCond->H().nx, initCond->H().ny), cl::NullRange, 0, &event));
+    CL_CHECK(event.wait());
+
+    // ...
 }
 
 Simulator::Simulator(const OptionsPtr &options, const InitCondPtr &initCond)
@@ -26,7 +60,17 @@ Simulator::~Simulator()
 
 bool Simulator::_init()
 {
-    // ### nothing here yet
+    // initialize OpenCL structures
+    vector<pair<string, string> > sources;
+    sources.push_back(make_pair("ReconstructH", "reconstructH.cl"));
+    OpenCLUtils::init(
+                sources,
+                (boost::format("-D NX=%d -D NY=%d") % options()->nx() % options()->ny()).str(),
+                options()->cpu() ? CL_DEVICE_TYPE_CPU : CL_DEVICE_TYPE_GPU);
+
+    // reconstruct H
+    pimpl->reconstructH(initCond());
+
     return true;
 }
 
@@ -42,7 +86,9 @@ double Simulator::_maxTime() const
 
 void Simulator::_execNextStep()
 {
-    // ### nothing here yet
+    // compute U ... TBD
+    // compute V ... TBD
+    // compute eta ... TBD
 }
 
 FieldInfo Simulator::_U() const
