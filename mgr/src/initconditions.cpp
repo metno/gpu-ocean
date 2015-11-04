7dd499e27a35fecbce91da3a3a41ecd41404d254
Martin Lilleeng Sætra <martinls@met.no>
2015-11-04 16:02:48 +0100

    Implemented generation/computation of H and eta in initial conditions.

--- mgr/src/initconditions.cpp
+++ mgr/src/initconditions.cpp
@@ -219,9 +219,9 @@ inline FieldInfo generateWaterElevation(int no, int nx, int ny, float width, flo
 			for (unsigned int i=0; i<nx; ++i) {
 				float x = (i+0.5f) / (float) nx - 0.5f;
 				if ((x*x)+(y*y)<0.01f)
-					f[j*nx+i] = 1.0f;
+					f[j*nx+i] = 1.5f;
 				else
-					f[j*nx+i] = 0.1f;
+					f[j*nx+i] = 1.0f;
 			}
 		}
 		break;
@@ -242,14 +242,139 @@ inline FieldInfo generateWaterElevation(int no, int nx, int ny, float width, flo
     return FieldInfo(f_, nx, ny, dx, dy);
 }
 
+inline FieldInfo generateH(int nx, int ny, float width, float height, FieldInfo B, float w=1.0f)
+{
+    const float dx = width / (nx - 1);
+    const float dy = height / (ny - 1);
+
+	if (nx <= 0 || ny <= 0) {
+		cout << "Invalid nx or ny: [" << nx << ", " << ny << "]." << endl;
+		exit(-1);
+	}
+
+	cout << "Generating sea surface mean depth (H): '";
+
+	shared_ptr<vector<float> > f_;
+	f_.reset(new vector<float>(nx+1, ny+1));
+	vector<float> f = *f_;
+
+#pragma omp parallel for
+	for (int i = 0; i<static_cast<int>(nx*ny); ++i) {
+		f[i] = w - B.data->at(i);
+		if (f[i] < 0.0f) {
+			cout << "Negative values in H are not allowed. (Increase global water elevation (w) or change bathymetry (B) input.)" << endl;
+			exit(-1);
+		}
+	}
+
+	cout << "' (" << nx << "x" << ny << " values)" << endl;
+
+    return FieldInfo(f_, nx, ny, dx, dy);
+}
+
+inline FieldInfo generateEta(int no, int nx, int ny, float width, float height)
+{
+    const float dx = width / (nx - 1);
+    const float dy = height / (ny - 1);
+
+	if (nx <= 0 || ny <= 0) {
+		cout << "Invalid nx or ny: [" << nx << ", " << ny << "]." << endl;
+		exit(-1);
+	}
+
+	cout << "Generating sea surface deviation (eta): '";
+
+	shared_ptr<vector<float> > f_;
+	f_.reset(new vector<float>(nx+1, ny+1));
+	vector<float> f = *f_;
+
+	switch (no)
+	{
+	case 0:
+		cout << "column";
+#pragma omp parallel for
+		for (int j=0; j<static_cast<int>(ny); ++j) {
+			float y = (j+0.5f) / (float) ny - 0.5f;
+			for (unsigned int i=0; i<nx; ++i) {
+				float x = (i+0.5f) / (float) nx - 0.5f;
+				if ((x*x)+(y*y)<0.01f)
+					f[j*nx+i] = 1.0f;
+				else
+					f[j*nx+i] = 0.0f;
+			}
+		}
+		break;
+
+	case 1:
+		cout << "gaussian";
+#pragma omp parallel for
+		for (int j=0; j<static_cast<int>(ny); ++j) {
+			float y = (j+0.5f) / (float) ny - 0.5f;
+			for (unsigned int i=0; i<nx; ++i) {
+				float x = (i+0.5f) / (float) nx - 0.5f;
+				f[j*nx+i] = exp(-(x*x) - (y*y));
+			}
+		}
+		break;
+
+	case 2:
+		cout << "zero";
+#pragma omp parallel for
+		for (int i=0; i<static_cast<int>(nx*ny); ++i)
+			f[i] = 0.0f;
+		break;
+
+
+	case STEADY_FLOW_OVER_BUMP:
+		cout << "Steady Flow Over Bump";
+#pragma omp parallel for
+		for (int i = 0; i<static_cast<int>(nx*ny); ++i)
+			f[i] = 0.0f;
+		break;
+
+	case IDEALISED_CIRCULAR_DAM:
+		cout << "Idealised Circular Dam";
+    {
+        const float x1 = width * 0.5f;
+        const float y1 = height * 0.5f;
+        const float v1 = width * 0.1f;
+        const float v2 = width * 0.065f;
+#pragma omp parallel for
+        for (int j=0; j<static_cast<int>(ny); ++j) {
+            float y = dy*(j+0.5f)-y1;
+            for (unsigned int i=0; i<nx; ++i) {
+                float x = dx*(i+0.5f)-x1;
+                if (sqrt(x*x+y*y) < v2)
+                    f[j*nx+i] = v1;
+                else
+                    f[j*nx+i] = 0.0f;
+            }
+        }
+    }
+        break;
+
+	default:
+		cout << "Could not recognize " << no << " as a valid id." << endl;
+		exit(-1);
+	}
+
+	cout << "' (" << nx << "x" << ny << " values)" << endl;
+
+    return FieldInfo(f_, nx, ny, dx, dy);
+}
+
 void InitConditions::init(const OptionsPtr &options)
 {
 	if(options->waterElevationNo() >= 0 && options->bathymetryNo() >= 0) {
 		///XXX: Maybe we should move the data generation outside of this class?
-        pimpl->waterElevationField = generateWaterElevation(options->waterElevationNo(), options->nx(), options->ny(), options->width(), options->height());
-        pimpl->bathymetryField = generateBathymetry(options->bathymetryNo(), options->nx(), options->ny(), options->width(), options->height());
 
-        // compute H and eta ... TBD
+		///XXX: We don't really need this field to initialize a simulation run for time being, but maybe later...
+		//pimpl->waterElevationField = generateWaterElevation(options->waterElevationNo(), options->nx(), options->ny(), options->width(), options->height());
+
+		pimpl->bathymetryField = generateBathymetry(options->bathymetryNo(), options->nx(), options->ny(), options->width(), options->height());
+        pimpl->H = generateH(options->nx(), options->ny(), options->width(), options->height(), pimpl->bathymetryField, 1.0f);
+
+        pimpl->eta = generateEta(options->bathymetryNo(), options->nx(), options->ny(), options->width(), options->height());
 	}
 }
 
