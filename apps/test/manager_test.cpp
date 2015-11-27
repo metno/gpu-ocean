5e4dcaf8a2854c4a5b770157e7bef4d58030ab7e
Jo Asplin <jo.asplin@met.no>
2015-11-27 16:15:48 +0100

    Fixed test

--- apps/test/manager_test.cpp
+++ apps/test/manager_test.cpp
@@ -1,19 +1,42 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE mgr test
 #include <boost/test/unit_test.hpp>
-
 #define BOOST_TEST_LOG_LEVEL "all"
 
 #include "manager.h"
 #include "programoptions.h"
 #include "simulator.h"
+#include <vector>
+#include <string>
+#include <utility>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+// Returns an (argc, argv) pair from the words in a string. The word "argv0" is automatically prepended.
+static pair<int, char **> s2args(const string &s)
+{
+    vector<string> words;
+    boost::split(words, s, boost::is_any_of("\t "));
+    words.insert(words.begin(), "argv0");
+    const int argc = words.size();
+    char **argv = (char **)malloc(argc * sizeof(char *));
+    for (int i = 0; i < words.size(); ++i) {
+        *(argv + i) = (char *)malloc(words.at(i).size() + 1);
+        strcpy(*(argv + i), words.at(i).c_str());
+    }
+
+    return make_pair(argc, argv);
+}
+
 
 struct ManagerFixture {
 	ManagerFixture()
 	{
 		BOOST_TEST_MESSAGE( "setup fixture" );
 
-		Manager::init(0, NULL);
+        pair<int, char **> args = s2args("--duration 0 --nx 2 --ny 2 --width 1 --height 1 --bathymetryNo 0 --etaNo 0");
+        Manager::init(args.first, args.second);
 
 		manager = &Manager::instance();
 	}
