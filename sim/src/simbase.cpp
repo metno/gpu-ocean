b7eb209191d7a57b1eb6ec288bd3e5f0cce3af51
Jo Asplin <jo.asplin@met.no>
2015-12-04 14:04:00 +0100

    Moved some documentation to headers

--- sim/src/simbase.cpp
+++ sim/src/simbase.cpp
@@ -21,11 +21,6 @@ SimBase::SimBaseImpl::SimBaseImpl(const OptionsPtr &options, const InitCondPtr &
 {
 }
 
-/**
- * Constructor.
- * @param options Input: Options.
- * @param initCond Input: Initial conditions.
- */
 SimBase::SimBase(const OptionsPtr &options, const InitCondPtr &initCond)
     : pimpl(new SimBaseImpl(options, initCond))
 {
@@ -41,50 +36,29 @@ void SimBase::assertInitialized() const
         throw runtime_error("SimBase: not initialized");
 }
 
-/**
- * Initializes the simulator. This includes resetting the current and final step values.
- * @returns Initialization status (true iff initialization was successful)
- */
 bool SimBase::init()
 {
     return pimpl->isInit = _init();
 }
 
-/**
- * Returns the current simulation time in seconds.
- */
 double SimBase::currTime() const
 {
     assertInitialized();
     return _currTime();
 }
 
-/**
- * Returns the maximum simulation time in seconds.
- */
 double SimBase::maxTime() const
 {
     assertInitialized();
     return _maxTime();
 }
 
-/**
- * Returns the time (in seconds) by which to advance the simulation in each step.
- */
 float SimBase::deltaTime() const
 {
     assertInitialized();
     return _deltaTime();
 }
 
-/**
- * Executes the next simulation step and returns true if the simulation is not time-bounded
- * or the current simulation time is still less than the maximum simulation time.
- * Otherwise, the function returns false without executing the next simulation step.
- * Note: The simulation is time-bounded iff the program option 'duration' is non-negative.
- * @param profInfo: If non-null, structure in which profiling is written (if applicable).
- * @returns true iff a time-bounded simulation was not exhausted
- */
 bool SimBase::execNextStep(ProfileInfo *profInfo)
 {
     assertInitialized();
@@ -97,71 +71,47 @@ bool SimBase::execNextStep(ProfileInfo *profInfo)
     return true;
 }
 
-/**
- * Returns U at the current simulation time.
- */
-Field2D SimBase::U() const
-{
-    assertInitialized();
-    return _U();
-}
-
-/**
- * Returns V at the current simulation time.
- */
-Field2D SimBase::V() const
-{
-    assertInitialized();
-    return _V();
-}
-
-/**
- * Returns eta at the current simulation time.
- */
 Field2D SimBase::eta() const
 {
     assertInitialized();
     return _eta();
 }
 
-/**
- * Returns the friction.
- */
+Field2D SimBase::U() const
+{
+    assertInitialized();
+    return _U();
+}
+
+Field2D SimBase::V() const
+{
+    assertInitialized();
+    return _V();
+}
+
 float SimBase::F() const
 {
     assertInitialized();
     return _F();
 }
 
-/**
- * Returns the Coriolis effect.
- */
 float SimBase::R() const
 {
     assertInitialized();
     return _R();
 }
 
-/**
- * Prints status.
- */
 void SimBase::printStatus() const
 {
     assertInitialized();
     _printStatus();
 }
 
-/**
- * Returns the options.
- */
 OptionsPtr SimBase::options() const
 {
     return pimpl->options;
 }
 
-/**
- * Returns the initial conditions.
- */
 InitCondPtr SimBase::initCond() const
 {
     return pimpl->initCond;
--- sim/src/simbase.h
+++ sim/src/simbase.h
@@ -15,22 +15,87 @@
 class SimBase
 {
 public:
+
+    /**
+     * Initializes the simulator. This includes resetting the current and final step values.
+     * @returns Initialization status (true iff initialization was successful).
+     */
     bool init();
+
+    /**
+     * Returns the current simulation time in seconds.
+     */
     double currTime() const;
+
+    /**
+     * Returns the maximum simulation time in seconds.
+     */
     double maxTime() const;
+
+    /**
+     * Returns the time (in seconds) by which to advance the simulation in each step.
+     */
     float deltaTime() const;
-    bool execNextStep(ProfileInfo * = 0);
-    Field2D U() const;
-    Field2D V() const;
+
+    /**
+     * Executes the next simulation step and returns true if the simulation is not time-bounded
+     * or the current simulation time is still less than the maximum simulation time.
+     * Otherwise, the function returns false without executing the next simulation step.
+     * Note: The simulation is time-bounded iff the program option 'duration' is non-negative.
+     * @param profInfo: If non-null, structure in which profiling is written (if applicable).
+     * @returns True iff a time-bounded simulation was not exhausted.
+     */
+    bool execNextStep(ProfileInfo *profInfo = 0);
+
+    /**
+     * Returns the sea surface deviation away from the equilibrium depth at the current simulation time.
+     */
     Field2D eta() const;
+
+    /**
+     * Returns the depth averaged velocity in the x direction at the current simulation time.
+     */
+    Field2D U() const;
+
+    /**
+     * Returns the depth averaged velocity in the y direction at the current simulation time.
+     */
+    Field2D V() const;
+
+    /**
+     * Returns the friction.
+     */
     float F() const;
+
+    /**
+     * Returns the Coriolis effect.
+     */
     float R() const;
+
+    /**
+     * Prints status.
+     */
     void printStatus() const;
+
+    /**
+     * Returns the program options.
+     */
     OptionsPtr options() const;
+
+    /**
+     * Returns the initial conditions.
+     */
     InitCondPtr initCond() const;
 
 protected:
-    SimBase(const OptionsPtr &, const InitCondPtr &);
+
+    /**
+     * Constructor.
+     * @param options: Program options.
+     * @param initCond: Initial conditions.
+     */
+    SimBase(const OptionsPtr &options, const InitCondPtr &initCond);
+
     virtual ~SimBase();
     virtual bool _init() = 0;
     virtual double _currTime() const = 0;
