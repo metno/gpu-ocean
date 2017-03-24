d78bcad25611d8a5ecce6ab3ed6e45c74c3afde7
Havard Heitlo Holm <havard.heitlo.holm@sintef.no>
2017-03-24 11:26:23 +0100

    Fixed tests (to almost equal) and added CTCS test class

--- doc/reference_python/tests/schemes/CTCStest.py
+++ doc/reference_python/tests/schemes/CTCStest.py
@@ -6,9 +6,9 @@ from testUtils import *
 
 import sys
 sys.path.insert(0, '../')
-from SWESimulators import FBL
+from SWESimulators import CTCS
 
-class FBLtest(unittest.TestCase):
+class CTCStest(unittest.TestCase):
 
     def setUp(self):
         self.cl_ctx = make_cl_ctx()
@@ -23,26 +23,27 @@ class FBLtest(unittest.TestCase):
         self.g = 9.81
         self.f = 0.0
         self.r = 0.0
-
-        self.h0 = np.ones((self.ny, self.nx), dtype=np.float32) * 60;
-        self.eta0 = np.zeros((self.ny, self.nx), dtype=np.float32);
-        self.u0 = np.zeros((self.ny, self.nx+1), dtype=np.float32);
-        self.v0 = np.zeros((self.ny+1, self.nx), dtype=np.float32);
+        self.A = 1
+        
+        self.h0 = np.ones((self.ny+2, self.nx+2), dtype=np.float32) * 60;
+        self.eta0 = np.zeros((self.ny+2, self.nx+2), dtype=np.float32);
+        self.u0 = np.zeros((self.ny+2, self.nx+1), dtype=np.float32);
+        self.v0 = np.zeros((self.ny+1, self.nx+2), dtype=np.float32);
 
         self.T = 50.0
         
 
     def test_wall_central(self):
-        makeCentralBump(self.eta0, self.nx, self.ny, self.dx, self.dy, 0, 0)
-        sim = FBL.FBL(self.cl_ctx, \
-                      self.h0, self.eta0, self.u0, self.v0, \
-                      self.nx, self.ny, \
-                      self.dx, self.dy, self.dt, \
-                      self.g, self.f, self.r)
+        makeCentralBump(self.eta0, self.nx, self.ny, self.dx, self.dy, 1, 1)
+        sim = CTCS.CTCS(self.cl_ctx, \
+                        self.h0, self.eta0, self.u0, self.v0, \
+                        self.nx, self.ny, \
+                        self.dx, self.dy, self.dt, \
+                        self.g, self.f, self.r, self.A)
         
         t = sim.step(self.T)
         eta1, u1, v1 = sim.download()
-        eta2, u2, v2 = loadResults("FBL", "wallBC", "central")
+        eta2, u2, v2 = loadResults("CTCS", "wallBC", "central")
 
         diffEta = np.linalg.norm(eta1 - eta2)
         diffU = np.linalg.norm(u1-u2)
@@ -50,6 +51,7 @@ class FBLtest(unittest.TestCase):
 
         self.assertAlmostEqual(diffEta, 0.0,
                                msg='Unexpected eta - L2 difference: ' + str(diffEta))
+
         self.assertAlmostEqual(diffU, 0.0,
                                msg='Unexpected U - L2 difference: ' + str(diffU))
         self.assertAlmostEqual(diffV, 0.0,
@@ -58,16 +60,16 @@ class FBLtest(unittest.TestCase):
 
 
     def test_wall_corner(self):
-        makeCornerBump(self.eta0, self.nx, self.ny, self.dx, self.dy, 0, 0)
-        sim = FBL.FBL(self.cl_ctx, \
-                      self.h0, self.eta0, self.u0, self.v0, \
-                      self.nx, self.ny, \
-                      self.dx, self.dy, self.dt, \
-                      self.g, self.f, self.r)
+        makeCornerBump(self.eta0, self.nx, self.ny, self.dx, self.dy, 1, 1)
+        sim = CTCS.CTCS(self.cl_ctx, \
+                        self.h0, self.eta0, self.u0, self.v0, \
+                        self.nx, self.ny, \
+                        self.dx, self.dy, self.dt, \
+                        self.g, self.f, self.r, self.A)
         
         t = sim.step(self.T)
         eta1, u1, v1 = sim.download()
-        eta2, u2, v2 = loadResults("FBL", "wallBC", "corner")
+        eta2, u2, v2 = loadResults("CTCS", "wallBC", "corner")
 
         diffEta = np.linalg.norm(eta1 - eta2)
         diffU = np.linalg.norm(u1-u2)
@@ -75,6 +77,7 @@ class FBLtest(unittest.TestCase):
 
         self.assertAlmostEqual(diffEta, 0.0,
                                msg='Unexpected eta - L2 difference: ' + str(diffEta))
+
         self.assertAlmostEqual(diffU, 0.0,
                                msg='Unexpected U - L2 difference: ' + str(diffU))
         self.assertAlmostEqual(diffV, 0.0,
