1788b21067b7b86f1c5931e0914de3eb4dca1a29
Havard Heitlo Holm <havard.heitlo.holm@sintef.no>
2017-07-04 16:44:13 +0200

    Added tests for closed boundaries and KP07

--- doc/reference_python/tests/oneLayer_2D_tests.py
+++ doc/reference_python/tests/oneLayer_2D_tests.py
@@ -10,11 +10,24 @@ import xmlrunner
 from schemes.FBLtest import FBLtest
 from schemes.CTCStest import CTCStest
 from schemes.CDKLM16test import CDKLM16test
+from schemes.KP07test import KP07test
+
+def printSupportedSchemes():
+    print ("Supported schemes:")
+    print ("1: FBL, 2: CTCS, 3: CDKLM16, 4: KP07")
+    
+
+if (len(sys.argv) < 2):
+    print ("Usage:")
+    print ("\t %s scheme [jenkins]" % sys.argv[0])
+    printSupportedSchemes()
+    exit()
+scheme = int(sys.argv[1])
 
 # In order to format the test report so that Jenkins can read it:
 jenkins = False
-if (len(sys.argv) > 1):
-    if (sys.argv[1].lower() == "jenkins"):
+if (len(sys.argv) > 2):
+    if (sys.argv[2].lower() == "jenkins"):
         jenkins = True
 
 if (jenkins):
@@ -22,8 +35,20 @@ if (jenkins):
 
 
 # Define the tests that will be part of our test suite:
-#test_classes_to_run = [FBLtest, CTCStest] #, CDKLM16test]
-test_classes_to_run = [CDKLM16test]
+test_classes_to_run = None
+if scheme == 1:
+    test_classes_to_run = [FBLtest]
+elif scheme == 2:
+    test_classes_to_run = [CTCStest]
+elif scheme == 3:
+    test_classes_to_run = [CDKLM16test]
+elif scheme == 4:
+    test_classes_to_run = [KP07test]
+else:
+    print("Error: " + str(scheme) + " is not a supported scheme...")
+    printSupportedSchemes()
+    exit()
+
 
 loader = unittest.TestLoader()
 suite_list = []
