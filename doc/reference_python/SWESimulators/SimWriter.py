4d7896d64838b8fd581dec055c25078faf71b749
Havard Heitlo Holm <havard.heitlo.holm@sintef.no>
2017-09-13 13:52:07 +0200

    Writing first timestep to netCDF successfully

--- doc/reference_python/SWESimulators/SimWriter.py
+++ doc/reference_python/SWESimulators/SimWriter.py
@@ -42,15 +42,19 @@ class SimNetCDFWriter:
 
         # Identification of solution
         self.timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+        self.timestamp_short = datetime.datetime.now().strftime("%Y_%m_%d")
         self.git_hash = str.strip(subprocess.check_output(['git', 'rev-parse', 'HEAD']))
         self.simulator_long = str(sim.__class__)
         self.simulator_short = (self.simulator_long.split("."))[-1]
-        self.output_file_name = self.simulator_short + "_" + self.timestamp + ".nc"
+
+        self.dir_name = "netcdf_" + self.timestamp_short + "/"
+        self.output_file_name = self.dir_name + self.simulator_short + "_" + self.timestamp + ".nc"
 
         self.current_directory =os.getcwd()
         self.textPos = -1
         
         # Simulator info
+        self.sim = sim
         self.boundary_conditions = str(sim.boundary_conditions)
         self.init_conditions_h = sim.h_init
         self.init_conditions_u = sim.hu_init/sim.h_init
@@ -61,53 +65,109 @@ class SimNetCDFWriter:
         self.minmod_theta = sim.theta
         self.coriolis_force = sim.f
         self.wind_stress = sim.wind_stress
-        self.g = sim.g
-        self.nx = sim.nx
-        self.ny = sim.ny
-        self.dx = sim.dx
-        self.dy = sim.dy
+        g = sim.g
+        nx = sim.nx
+        ny = sim.ny
+        dx = sim.dx
+        dy = sim.dy
+        self.ghost_cells_x = sim.ghost_cells_x
+        self.ghost_cells_y = sim.ghost_cells_y
         self.bottom_friction_r = sim.r
-        
 
-        # Arrays
-        #self.h = ""
-        #self.u = ""
-        #self.v = ""
-        #self.t = ""
+        # Organize directory and create file:
+        if not os.path.isdir(self.dir_name):
+            os.makedirs(self.dir_name)
+        self.ncfile = Dataset(self.output_file_name,'w', clobber=True) 
 
-        # Init conditions should be added as the first element in the above arrays!
+        #Create dimensions 
+        self.ncfile.createDimension('time', None) #Unlimited time dimension
+        if (not self.ignore_ghostcells) and (self.staggered_grid):
+            self.ncfile.createDimension('x_eta', nx + 2*self.ghost_cells_x)
+            self.ncfile.createDimension('y_eta', ny + 2*self.ghost_cells_y)
+            self.ncfile.createDimension('x_u',   nx + 2*self.ghost_cells_x - 1)
+            self.ncfile.createDimension('y_u',   ny + 2*self.ghost_cells_y)
+            self.ncfile.createDimension('x_v',   nx + 2*self.ghost_cells_x)
+            self.ncfile.createDimension('y_v',   ny + 2*self.ghost_cells_y - 1)
+        elif (not self.ignore_ghostcells) and (not self.staggered_grid):
+            self.ncfile.createDimension('x', nx + 2*self.ghost_cells_x)
+            self.ncfile.createDimension('y', ny + 2*self.ghost_cells_y)
+        #Create axis
+        self.nc_time = self.ncfile.createVariable('time', np.dtype('float32').char, 'time')
+        x = self.ncfile.createVariable('x', np.dtype('float32').char, 'x')
+        y = self.ncfile.createVariable('y', np.dtype('float32').char, 'y')
 
+        x.standard_name = "projection_x_coordinate"
+        y.standard_name = "projection_y_coordinate"
+        x.axis = "X"
+        y.axis = "Y"
 
-    def _addText(self, ax, msg):
-        bp = 70 # breakpoint
-        if len(msg) > bp:
-            rest = '     ' + msg[bp:]
-            ax.text(0.1, self.textPos, msg[0:bp])
-            self.textPos -= 0.2
-            self._addText(ax, rest)
+        #Create bogus projection variable
+        self.nc_proj = self.ncfile.createVariable('projection_stere', np.dtype('int32').char)
+        self.nc_proj.grid_mapping_name = 'polar_stereographic'
+        self.nc_proj.scale_factor_at_projection_origin = 0.9330127018922193
+        self.nc_proj.straight_vertical_longitude_from_pole = 70.0
+        self.nc_proj.latitude_of_projection_origin = 90.0
+        self.nc_proj.earth_radius = 6371000.0
+        self.nc_proj.proj4 = '+proj=stere +lat_0=90 +lon_0=70 +lat_ts=60 +units=m +a=6.371e+06 +e=0 +no_defs'
+
+        if not self.ignore_ghostcells and self.staggered_grid:
+            x_eta[:] = np.linspace(-dx/2.0, nx*dx + dx/2.0, nx+2)
+            y_eta[:] = np.linspace(-dy/2.0, ny*dy + dy/2.0, ny+2)
+            x_u[:] = np.linspace(0, nx*dx, nx+1)
+            y_u[:] = np.linspace(-dy/2.0, ny*dy + dy/2.0, ny+2)
+            x_v[:] = np.linspace(-dx/2.0, nx*dx + dx/2.0, nx+2)
+            y_v[:] = np.linspace(0, ny*dy, ny+1)
+        elif not self.ignore_ghostcells and not self.staggered_grid:
+            x[:] = np.linspace(-self.ghost_cells_x*dx + dx/2.0, \
+                               (nx + self.ghost_cells_x)*dx - dx/2.0, \
+                               nx + 2*self.ghost_cells_x)
+            y[:] = np.linspace(-self.ghost_cells_y*dy + dy/2.0, \
+                               (ny + self.ghost_cells_y)*dy - dy/2.0, \
+                               ny + 2*self.ghost_cells_y)
+
+        #Set units
+        self.nc_time.units = 'seconds since 1970-01-01 00:00:00'
+        if self.staggered_grid:
+            x_eta.units = 'meter'
+            y_eta.units = 'meter'
+            x_u.units = 'meter'
+            y_u.units = 'meter'
+            x_v.units = 'meter'
+            y_v.units = 'meter'
         else:
-            ax.text(0.1, self.textPos, msg)
-            #print len(msg)
-            self.textPos -= 0.2
-        
-    def infoPlot(self, ax):
-        self.textPos = 2.3
-        # Ax is the subplot object
-        ax.text(1, 2.8, 'NetCDF INFO')
-        
-        self._addText(ax, 'working directory: ' + self.current_directory)
-        self._addText(ax, 'filename: ' + self.output_file_name)
-        self._addText(ax, '')
-        self._addText(ax, 'git hash: ' + self.git_hash)
-        self._addText(ax, '')
-        self._addText(ax, 'Simulator: ' + self.simulator_short)
-        self._addText(ax, 'BC: ' + str(self.boundary_conditions))
-        self._addText(ax, 'f:  ' + str(self.coriolis_force))
-        self._addText(ax, 'dt: ' + str(self.dt))
-        self._addText(ax, 'wind type: ' + str(self.wind_stress.type))
-        
-        ax.axis([0, 6, 0, 3])
+            x.units = 'meter'
+            y.units = 'meter'
 
+        #Create a land mask (with no land)
+        self.nc_land = self.ncfile.createVariable('land_binary_mask', np.dtype('float32').char, ('y', 'x'))
+        self.nc_land.standard_name = 'land_binary_mask'
+        self.nc_land.units = '1'
+        self.nc_land[:] = 0
+
+        self.nc_eta = self.ncfile.createVariable('eta', np.dtype('float32').char, ('time', 'y', 'x'), zlib=True)
+        self.nc_u = self.ncfile.createVariable('u', np.dtype('float32').char, ('time', 'y', 'x'), zlib=True)
+        self.nc_v = self.ncfile.createVariable('v', np.dtype('float32').char, ('time', 'y', 'x'), zlib=True)
+        self.nc_eta.standard_name = 'sea_water_elevation'
+        self.nc_u.standard_name = 'x_sea_water_velocity'
+        self.nc_v.standard_name = 'y_sea_water_velocity'
+        self.nc_eta.grid_mapping = 'projection_stere'
+        self.nc_u.grid_mapping = 'projection_stere'
+        self.nc_v.grid_mapping = 'projection_stere'
+        self.nc_eta.coordinates = 'lon lat'
+        self.nc_u.coordinates = 'lon lat'
+        self.nc_v.coordinates = 'lon lat'
+
+        #Set units
+        self.nc_eta.units = 'meter'
+        self.nc_u.units = 'meter second-1'
+        self.nc_v.units = 'meter second-1'
+ 
+        # Init conditions should be added as the first element in the above arrays!
+        self.i = 0
+        self.writeTimestep(sim)
+
+
+    #--------------------------------------------------------------___#
         
     def oldInit(self, outfilename, nx, ny, dx, dy, num_layers=1, ignore_ghostcells=True, \
                 width=1, height=1):
@@ -253,33 +313,76 @@ class SimNetCDFWriter:
         
         
     def __exit__(self, exc_type, exc_value, traceback):
-        print "Closing '" + self.ncfile.filepath() + "'"
+        print "Closing file..." # '" + self.ncfile.filepath() + "'"
         self.ncfile.close()
         
         
         
-        
-        
-    def write(self, i, t, eta, u, v, eta2=None, u2=None, v2=None):
+
+    def writeTimestep(self, sim):
+        eta, hu, hv = sim.download()
+        if (not self.ignore_ghostcells):
+            self.nc_time[self.i] = sim.t
+            self.nc_eta[self.i, :] = eta
+            self.nc_u[self.i, :] = hu
+            self.nc_v[self.i, :] = hv
+            print("TIMESTEP WRITTEN!")
+        self.i += 1
+
+            
+    def write(self, t, eta, u, v, eta2=None, u2=None, v2=None):
         if (self.ignore_ghostcells):
-            self.nc_time[i] = t
+            self.nc_time[self.i] = t
             #self.nc_eta[i, :] = eta[1:-1, 1:-1]
             #self.nc_u[i, :] = u[1:-1, 1:-1]
             #self.nc_v[i, :] = v[1:-1, 1:-1]
-            self.nc_u[i, :] = u[1:-2, 1:-1]
-            self.nc_v[i, :] = v[1:-1, 1:-2]
+            self.nc_u[self.i, :] = u[1:-2, 1:-1]
+            self.nc_v[self.i, :] = v[1:-1, 1:-2]
         else:
-            self.nc_time[i] = t
-            self.nc_eta[i, :] = eta
-            self.nc_u[i, :] = u
-            self.nc_v[i, :] = v
+            self.nc_time[self.i] = t
+            self.nc_eta[self.i, :] = eta
+            self.nc_u[self.i, :] = u
+            self.nc_v[self.i, :] = v
             
         if(self.num_layers == 2):
             if (self.ignore_ghostcells):
-                self.nc_eta2[i, :] = eta2[1:-1, 1:-1]
-                self.nc_u2[i, :] = u2[1:-1, 1:-1]
-                self.nc_v2[i, :] = v2[1:-1, 1:-1]
+                self.nc_eta2[self.i, :] = eta2[1:-1, 1:-1]
+                self.nc_u2[self.i, :] = u2[1:-1, 1:-1]
+                self.nc_v2[self.i, :] = v2[1:-1, 1:-1]
             else:
-                self.nc_eta2[i, :] = eta2
-                self.nc_u2[i, :] = u2
-                self.nc_v2[i, :] = v2
+                self.nc_eta2[self.i, :] = eta2
+                self.nc_u2[self.i, :] = u2
+                self.nc_v2[self.i, :] = v2
+
+        self.i += 1
+
+
+    def _addText(self, ax, msg):
+        bp = 70 # breakpoint
+        if len(msg) > bp:
+            rest = '     ' + msg[bp:]
+            ax.text(0.1, self.textPos, msg[0:bp])
+            self.textPos -= 0.2
+            self._addText(ax, rest)
+        else:
+            ax.text(0.1, self.textPos, msg)
+            #print len(msg)
+            self.textPos -= 0.2
+        
+    def infoPlot(self, ax):
+        self.textPos = 2.3
+        # Ax is the subplot object
+        ax.text(1, 2.8, 'NetCDF INFO')
+        
+        self._addText(ax, 'working directory: ' + self.current_directory)
+        self._addText(ax, 'filename: ' + self.output_file_name)
+        self._addText(ax, '')
+        self._addText(ax, 'git hash: ' + self.git_hash)
+        self._addText(ax, '')
+        self._addText(ax, 'Simulator: ' + self.simulator_short)
+        self._addText(ax, 'BC: ' + str(self.boundary_conditions))
+        self._addText(ax, 'f:  ' + str(self.coriolis_force))
+        self._addText(ax, 'dt: ' + str(self.dt) + ", dx: " + str(self.sim.dx) + ", dy: " + str(self.sim.dy))
+        self._addText(ax, 'wind type: ' + str(self.wind_stress.type))
+        
+        ax.axis([0, 6, 0, 3])
