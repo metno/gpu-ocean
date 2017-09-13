33bb385eb87ef4e99f6501852acff46004d15b48
Havard Heitlo Holm <havard.heitlo.holm@sintef.no>
2017-09-13 10:47:44 +0200

    Initial sketch of NetCDF writer and info plotter

---
+++ doc/reference_python/SWESimulators/SimWriter.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+
+"""
+This python module implements saving shallow water simulations to a
+netcdf file.
+
+Copyright (C) 2016  SINTEF ICT
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import numpy as np
+import datetime
+from netCDF4 import Dataset
+import subprocess
+import matplotlib.pyplot as plt
+import os as os
+
+class SimNetCDFWriter:
+    def __init__(self, sim, num_layers=1, staggered_grid=False, \
+                 ignore_ghostcells=False, \
+                 width=1, height=1):
+
+        # OpenCL queue:
+        self.cl_queue = sim.cl_queue
+
+        # Write options:
+        self.ignore_ghostcells = ignore_ghostcells
+        self.num_layers = num_layers
+        self.staggered_grid = staggered_grid
+
+        # Identification of solution
+        self.timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+        self.git_hash = str.strip(subprocess.check_output(['git', 'rev-parse', 'HEAD']))
+        self.simulator_long = str(sim.__class__)
+        self.simulator_short = (self.simulator_long.split("."))[-1]
+        self.output_file_name = self.simulator_short + "_" + self.timestamp + ".nc"
+
+        self.current_directory =os.getcwd()
+        self.textPos = -1
+        
+        # Simulator info
+        self.boundary_conditions = str(sim.boundary_conditions)
+        self.init_conditions_h = sim.h_init
+        self.init_conditions_u = sim.hu_init/sim.h_init
+        self.init_conditions_v = sim.hv_init/sim.h_init
+        self.dt = sim.dt
+        self.bathymetry = sim.bathymetry.download(self.cl_queue)[0]
+        self.time_integrator = sim.rk_order
+        self.minmod_theta = sim.theta
+        self.coriolis_force = sim.f
+        self.wind_stress = sim.wind_stress
+        self.g = sim.g
+        self.nx = sim.nx
+        self.ny = sim.ny
+        self.dx = sim.dx
+        self.dy = sim.dy
+        self.bottom_friction_r = sim.r
+        
+
+        # Arrays
+        #self.h = ""
+        #self.u = ""
+        #self.v = ""
+        #self.t = ""
+
+        # Init conditions should be added as the first element in the above arrays!
+
+
+    def _addText(self, ax, msg):
+        bp = 70 # breakpoint
+        if len(msg) > bp:
+            rest = '     ' + msg[bp:]
+            ax.text(0.1, self.textPos, msg[0:bp])
+            self.textPos -= 0.2
+            self._addText(ax, rest)
+        else:
+            ax.text(0.1, self.textPos, msg)
+            #print len(msg)
+            self.textPos -= 0.2
+        
+    def infoPlot(self, ax):
+        self.textPos = 2.3
+        # Ax is the subplot object
+        ax.text(1, 2.8, 'NetCDF INFO')
+        
+        self._addText(ax, 'working directory: ' + self.current_directory)
+        self._addText(ax, 'filename: ' + self.output_file_name)
+        self._addText(ax, '')
+        self._addText(ax, 'git hash: ' + self.git_hash)
+        self._addText(ax, '')
+        self._addText(ax, 'Simulator: ' + self.simulator_short)
+        self._addText(ax, 'BC: ' + str(self.boundary_conditions))
+        self._addText(ax, 'f:  ' + str(self.coriolis_force))
+        self._addText(ax, 'dt: ' + str(self.dt))
+        self._addText(ax, 'wind type: ' + str(self.wind_stress.type))
+        
+        ax.axis([0, 6, 0, 3])
+
+        
+    def oldInit(self, outfilename, nx, ny, dx, dy, num_layers=1, ignore_ghostcells=True, \
+                width=1, height=1):
+        self.ncfile = Dataset(outfilename,'w', clobber=True) 
+        self.ignore_ghostcells = ignore_ghostcells
+        self.num_layers = num_layers
+        
+        #Create dimensions 
+        self.ncfile.createDimension('time', None) #Unlimited time dimension
+        if (self.ignore_ghostcells):
+            #self.ncfile.createDimension('x_eta', nx)
+            #self.ncfile.createDimension('y_eta', ny)
+            #self.ncfile.createDimension('x_u', nx-1)
+            #self.ncfile.createDimension('y_u', ny)
+            #self.ncfile.createDimension('x_v', nx)
+            #self.ncfile.createDimension('y_v', ny-1)
+            self.ncfile.createDimension('x', nx-1)
+            self.ncfile.createDimension('y', ny-1)
+        else:
+            self.ncfile.createDimension('x_eta', nx+2)
+            self.ncfile.createDimension('y_eta', ny+2)
+            self.ncfile.createDimension('x_u', nx+1)
+            self.ncfile.createDimension('y_u', ny+2)
+            self.ncfile.createDimension('x_v', nx+2)
+            self.ncfile.createDimension('y_v', ny+1)
+
+        #Create axis
+        self.nc_time = self.ncfile.createVariable('time', np.dtype('float32').char, 'time')
+        #x_eta = self.ncfile.createVariable('x_eta', np.dtype('float32').char, 'x_eta')
+        #y_eta = self.ncfile.createVariable('y_eta', np.dtype('float32').char, 'y_eta')
+        #x_u = self.ncfile.createVariable('x_u', np.dtype('float32').char, 'x_u')
+        #y_u = self.ncfile.createVariable('y_u', np.dtype('float32').char, 'y_u')
+        #x_v = self.ncfile.createVariable('x_v', np.dtype('float32').char, 'x_v')
+        #y_v = self.ncfile.createVariable('y_v', np.dtype('float32').char, 'y_v')
+        x = self.ncfile.createVariable('x', np.dtype('float32').char, 'x')
+        y = self.ncfile.createVariable('y', np.dtype('float32').char, 'y')
+        
+        x.standard_name = "projection_x_coordinate"
+        y.standard_name = "projection_y_coordinate"
+        
+        x.axis = "X"
+        y.axis = "Y"
+        
+        #Create bogus projection variable
+        self.nc_proj = self.ncfile.createVariable('projection_stere', np.dtype('int32').char)
+        self.nc_proj.grid_mapping_name = 'polar_stereographic'
+        self.nc_proj.scale_factor_at_projection_origin = 0.9330127018922193
+        self.nc_proj.straight_vertical_longitude_from_pole = 70.0
+        self.nc_proj.latitude_of_projection_origin = 90.0
+        self.nc_proj.earth_radius = 6371000.0
+        self.nc_proj.proj4 = '+proj=stere +lat_0=90 +lon_0=70 +lat_ts=60 +units=m +a=6.371e+06 +e=0 +no_defs'
+        
+        #Set axis values/ticks
+        if (self.ignore_ghostcells):
+            #x_eta[:] = np.linspace(dx/2.0, nx*dx - dx/2.0, nx)
+            #y_eta[:] = np.linspace(dy/2.0, ny*dy - dy/2.0, ny)
+            #x_u[:] = np.linspace(1, (nx-1)*dx, nx-1)
+            #y_u[:] = np.linspace(dy/2.0, ny*dy - dy/2.0, ny)
+            #x_v[:] = np.linspace(dx/2.0, nx*dx - dx/2.0, nx)
+            #y_v[:] = np.linspace(1, (ny-1)*dy, ny-1)
+            x[:] = np.linspace(0, width, nx-1)
+            y[:] = np.linspace(0, height, ny-1)
+        else:
+            x_eta[:] = np.linspace(-dx/2.0, nx*dx + dx/2.0, nx+2)
+            y_eta[:] = np.linspace(-dy/2.0, ny*dy + dy/2.0, ny+2)
+            x_u[:] = np.linspace(0, nx*dx, nx+1)
+            y_u[:] = np.linspace(-dy/2.0, ny*dy + dy/2.0, ny+2)
+            x_v[:] = np.linspace(-dx/2.0, nx*dx + dx/2.0, nx+2)
+            y_v[:] = np.linspace(0, ny*dy, ny+1)
+            
+        #Set units
+        self.nc_time.units = 'seconds since 1970-01-01 00:00:00'
+        #x_eta.units = 'm'
+        #y_eta.units = 'm'
+        #x_u.units = 'm'
+        #y_u.units = 'm'
+        #x_v.units = 'm'
+        #y_v.units = 'm'
+        x.units = 'm'
+        y.units = 'm'
+
+        # Reference time does not make sense at this time, but at least we are following
+        # the CF conventions.
+        #self.nc_time.reference_time = date.today().isoformat()
+
+        #Create output data variables
+        #self.nc_eta = self.ncfile.createVariable('eta', np.dtype('float32').char, ('time', 'y_eta', 'x_eta'))
+        #self.nc_u = self.ncfile.createVariable('u', np.dtype('float32').char, ('time', 'y_u', 'x_u'))
+        #self.nc_v = self.ncfile.createVariable('v', np.dtype('float32').char, ('time', 'y_v', 'x_v'))
+        
+        #Create a land mask (with no land)
+        self.nc_land = self.ncfile.createVariable('land_binary_mask', np.dtype('float32').char, ('y', 'x'))
+        self.nc_land.standard_name = 'land_binary_mask'
+        self.nc_land.units = '1'
+        self.nc_land[:] = 0
+        
+        self.nc_u = self.ncfile.createVariable('u', np.dtype('float32').char, ('time', 'y', 'x'), zlib=True)
+        self.nc_v = self.ncfile.createVariable('v', np.dtype('float32').char, ('time', 'y', 'x'), zlib=True)
+        self.nc_u.standard_name = 'x_sea_water_velocity'
+        self.nc_v.standard_name = 'y_sea_water_velocity'
+        self.nc_u.grid_mapping = 'projection_stere'
+        self.nc_v.grid_mapping = 'projection_stere'
+        self.nc_u.coordinates = 'lon lat'
+        self.nc_v.coordinates = 'lon lat'
+        
+        if(num_layers == 2):
+            self.nc_eta2 = self.ncfile.createVariable('eta2', np.dtype('float32').char, ('time', 'y_eta', 'x_eta'))
+            self.nc_u2 = self.ncfile.createVariable('u2', np.dtype('float32').char, ('time', 'y_u', 'x_u'))
+            self.nc_v2 = self.ncfile.createVariable('v2', np.dtype('float32').char, ('time', 'y_v', 'x_v'))
+        
+        #Set units
+        #self.nc_eta.units = 'm'
+        #self.nc_u.units = 'm'
+        #self.nc_v.units = 'm'
+        self.nc_u.units = 'meter second-1'
+        self.nc_v.units = 'meter second-1'
+
+        
+    def __str__(self):
+        msg = ""
+        theMap = vars(self)
+        for i,j in theMap.items():
+            if str(type(j)) == '<type \'str\'>':
+                msg += i + ":\t\t" + j
+            elif str(type(j)) == '<type \'numpy.ndarray\'>':
+                msg += i + ":\t\tnumpy array of shape " + str(j.shape)
+            elif str(type(j)) == '<type \'numpy.int32\'>' or \
+                 str(type(j)) == '<type \'numpy.float32\'>' or \
+                 str(type(j)) == '<type \'int\'>' or \
+                 str(type(j)) == '<type \'bool\'>':
+                msg += i + ":\t\t" + str(j)
+            else:
+                msg += i + ":\t\t" + str(type(j)) 
+            msg += '\n'
+        return msg    
+        
+        
+    def __enter__(self):
+        return self
+        
+        
+        
+        
+        
+    def __exit__(self, exc_type, exc_value, traceback):
+        print "Closing '" + self.ncfile.filepath() + "'"
+        self.ncfile.close()
+        
+        
+        
+        
+        
+    def write(self, i, t, eta, u, v, eta2=None, u2=None, v2=None):
+        if (self.ignore_ghostcells):
+            self.nc_time[i] = t
+            #self.nc_eta[i, :] = eta[1:-1, 1:-1]
+            #self.nc_u[i, :] = u[1:-1, 1:-1]
+            #self.nc_v[i, :] = v[1:-1, 1:-1]
+            self.nc_u[i, :] = u[1:-2, 1:-1]
+            self.nc_v[i, :] = v[1:-1, 1:-2]
+        else:
+            self.nc_time[i] = t
+            self.nc_eta[i, :] = eta
+            self.nc_u[i, :] = u
+            self.nc_v[i, :] = v
+            
+        if(self.num_layers == 2):
+            if (self.ignore_ghostcells):
+                self.nc_eta2[i, :] = eta2[1:-1, 1:-1]
+                self.nc_u2[i, :] = u2[1:-1, 1:-1]
+                self.nc_v2[i, :] = v2[1:-1, 1:-1]
+            else:
+                self.nc_eta2[i, :] = eta2
+                self.nc_u2[i, :] = u2
+                self.nc_v2[i, :] = v2
