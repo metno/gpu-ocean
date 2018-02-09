c009486984b1cf817cd1bb1cd1ef42c82a19663c
Havard Heitlo Holm <havard.heitlo.holm@sintef.no>
2018-02-09 18:04:15 +0100

    Last tests during this round

--- python/tests/dataAssimilation/GlobalParticlesTest.py
+++ python/tests/dataAssimilation/GlobalParticlesTest.py
@@ -180,6 +180,83 @@ class GlobalParticlesTest(unittest.TestCase):
         self.assertGreaterEqual(np.min(p[:,1]) , 0.0)
         self.assertLessEqual(np.max(p[:,1]), domain_y)
 
-    #def test_copy(self):
+    def test_set_domain_size(self):
+        size_x = 10.3
+        size_y = 5.4
+        self.smallParticleSet.setDomainSize(size_x, size_y)
+
+        self.assertEqual(self.smallParticleSet.getDomainSizeX(), size_x)
+        self.assertEqual(self.smallParticleSet.getDomainSizeY(), size_y)
+
+        longDiag = np.sqrt(2*0.8*0.8)
+        longLine = 0.8
+        # Distance should now be the above, even with periodic boundary conditions
+        self.assertListAlmostEqual(self.smallParticleSet.getDistances().tolist(),
+                                   [longDiag, longLine, longLine], 12,
+                                   'getDistance() in big periodic domain')
         
+        
+    def test_copy(self):
+        size_x = 10.3
+        size_y = 5.4
+        self.smallParticleSet.setDomainSize(size_x, size_y)
+                        
+        # Give non-standard domain_size before 
+        copy = self.smallParticleSet.copy()
+        
+        self.assertEqual(copy.getNumParticles(), self.numParticles)
+        self.assertEqual(copy.getObservationVariance(), self.observationVariance)
+        
+        positions = copy.getParticlePositions()
+        self.assertEqual(positions.shape, ((self.numParticles, 2)))
+        self.assertEqual(positions[0,:].tolist(), [0.9, 0.9])
+        self.assertEqual(positions[1,:].tolist(), [0.9, 0.1])
+        self.assertEqual(positions[2,:].tolist(), [0.1, 0.9])
+        
+        observation = copy.getObservationPosition()
+        self.assertEqual(observation.shape, ((2,)))
+        self.assertEqual(observation.tolist(), [0.1, 0.1])
+        
+        self.assertEqual(copy.getBoundaryConditions().get(), [2,2,2,2])
+
+        self.assertEqual(copy.getDomainSizeX(), size_x)
+        self.assertEqual(copy.getDomainSizeY(), size_y)
+                                                           
+        # Move a particle in the original dataset and check that it is still the same in
+        # the copy
+
+        self.smallParticleSet.positions[1,0] = 0.5
+        self.smallParticleSet.positions[1,1] = 0.5
+
+        positions = copy.getParticlePositions()
+        self.assertEqual(positions[1,:].tolist(), [0.9, 0.1])
+
+
+    def test_copy_env(self):
+        size_x = 10.3
+        size_y = 5.4
+        self.smallParticleSet.setDomainSize(size_x, size_y)
+                        
+        # Give non-standard domain_size before
+        newNumParticles = 4
+        copy = self.smallParticleSet.copyEnv(newNumParticles)
+        
+        self.assertEqual(copy.getNumParticles(), newNumParticles)
+        self.assertEqual(copy.getObservationVariance(), self.observationVariance)
+        
+        positions = copy.getParticlePositions()
+        self.assertEqual(positions.shape, ((newNumParticles, 2)))
+        self.assertEqual(positions[0,:].tolist(), [0.0, 0.0])
+        self.assertEqual(positions[1,:].tolist(), [0.0, 0.0])
+        self.assertEqual(positions[2,:].tolist(), [0.0, 0.0])
+        self.assertEqual(positions[3,:].tolist(), [0.0, 0.0])
+        
+        observation = copy.getObservationPosition()
+        self.assertEqual(observation.shape, ((2,)))
+        self.assertEqual(observation.tolist(), [0.1, 0.1])
+        
+        self.assertEqual(copy.getBoundaryConditions().get(), [2,2,2,2])
+
+        self.assertEqual(copy.getDomainSizeX(), size_x)
+        self.assertEqual(copy.getDomainSizeY(), size_y)
         
