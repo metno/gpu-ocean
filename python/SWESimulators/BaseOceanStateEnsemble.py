Introducing ObservationType and fixing the confusion on variance vs standard deviation

python/SWESimulators/BaseOceanStateEnsemble.py
@@ -34,12 +34,11 @@ import WindStress
 import Common
 import DataAssimilationUtils as dautils
 
-
 class BaseOceanStateEnsemble(object):
 
     __metaclass__ = abc.ABCMeta
         
-    def __init__(self, numParticles, cl_ctx):
+    def __init__(self, numParticles, cl_ctx, observation_type=dautils.ObservationType.DrifterPosition):
         
         self.cl_ctx = cl_ctx
         
@@ -49,6 +48,10 @@ class BaseOceanStateEnsemble(object):
         self.obs_index = self.numParticles
         
         self.simType = 'CDKLM16'
+        
+        dautils.ObservationType._assert_valid(observation_type)
+        self.observation_type = observation_type
+        self.prev_observation = None
         
     def cleanUp(self):
         for oceanState in self.particles:
@@ -152,10 +155,9 @@ class BaseOceanStateEnsemble(object):
         # Resample and possibly perturb
         pass
         
-    
-    def observeParticles(self):
+    def observeDrifters(self):
         """
-        Applying the observation operator on each particle.
+        Observing the drifters in all particles
         """
         drifterPositions = np.empty((0,2))
         for oceanState in self.particles[:-1]:
@@ -164,13 +166,40 @@ class BaseOceanStateEnsemble(object):
                                          axis=0)
         return drifterPositions
     
-    def observeTrueState(self):
+    def observeParticles(self):
         """
-        Applying the observation operator on the syntetic true state,
-        adding an observation error.
+        Applying the observation operator on each particle.
+        """
+        if self.observation_type == dautils.ObservationType.DrifterPosition:
+            return self.observeDrifters()
+        
+        elif self.observation_type == dautils.ObservationType.UnderlyingFlow:
+            print "ObservationType.UnderlyingFlow"
+            drifterPositions = np.empty((0,2))
+            for oceanState in self.particles[:-1]:
+                drifterPositions = np.append(drifterPositions, 
+                                             oceanState.drifters.getDrifterPositions(),
+                                             #np.array([[10, 10]]),
+                                             axis=0)
+            return drifterPositions
+        
+    def observeTrueDrifters(self):
+        """
+        Observing the drifters in the syntetic true state.
         """
         observation = self.particles[self.obs_index].drifters.getDrifterPositions()
         return observation[0,:]
+        
+    def observeTrueState(self):
+        """
+        Applying the observation operator on the syntetic true state.
+        """
+        if self.observation_type == dautils.ObservationType.DrifterPosition:
+            return self.observeTrueDrifters()
+        
+        elif self.observation_type == dautils.ObservationType.UnderlyingFlow:
+            observation = self.particles[self.obs_index].drifters.getDrifterPositions()
+            return observation[0,:]
         
     def step(self, t, apply_stochastic_term=True):
         """

python/SWESimulators/DataAssimilationUtils.py
@@ -27,6 +27,20 @@ import time
 
 import Common
 
+class ObservationType:
+    """
+    An enum-type class for defining different types of observation operators.
+    """
+    DrifterPosition = 1
+    UnderlyingFlow = 2
+    
+    @staticmethod
+    def _assert_valid(obs_type):
+        assert(obs_type == ObservationType.DrifterPosition or \
+               obs_type == ObservationType.UnderlyingFlow), \
+        'Provided observation type ' + str(ot) + ' is invalid'
+
+
 
 def getGaussianWeight(distance, observationVariance, normalize=True):
     """
@@ -36,8 +50,8 @@ def getGaussianWeight(distance, observationVariance, normalize=True):
     observationVariance: the variance in the observation.
     """
     
-    weights = (1.0/np.sqrt(2*np.pi*observationVariance**2))* \
-            np.exp(- (distance**2/(2*observationVariance**2)))
+    weights = (1.0/np.sqrt(2*np.pi*observationVariance))* \
+            np.exp(- (distance**2/(2*observationVariance)))
     if normalize:
         return weights/np.sum(weights)
     return weights
@@ -52,7 +66,7 @@ def getCauchyWeight(distance, observationVariance, normalize=True):
     observationVariance: the variance in the observation.
     """
     
-    weights = 1.0/(np.pi*observationVariance*(1 + (distance/observationVariance)**2))
+    weights = 1.0/(np.pi*np.sqrt(observationVariance)*(1 + (distance**2/observationVariance)))
     if normalize:
         return weights/np.sum(weights)
     return weights