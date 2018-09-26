51e45abbc5a02a486211e878280319217f4b5a5e
Havard Heitlo Holm <havard.heitlo.holm@sintef.no>
2018-09-26 17:27:20 +0200

    Added test with central bump on top of exotic bathymetry

--- gpu_ocean/tests/schemes/CDKLM16_test.py
+++ gpu_ocean/tests/schemes/CDKLM16_test.py
@@ -350,3 +350,22 @@ class CDKLM16test(unittest.TestCase):
         eta2, u2, v2 = loadResults("CDKLM16", "coriolis", "central")
 
         self.checkResults(eta1, u1, v1, eta2, u2, v2)
+
+
+    def test_bathymetry_central(self):
+        self.setBoundaryConditions()
+        self.allocData() 
+        addCentralBump(self.eta0, self.nx, self.ny, self.dx, self.dy, self.validDomain)
+        makeBottomTopography(self.Hi, self.nx, self.ny, self.dx, self.dy, self.validDomain)
+        self.sim = CDKLM16.CDKLM16(self.gpu_ctx, \
+                                   self.eta0, self.u0, self.v0, self.Hi, \
+                                   self.nx, self.ny, \
+                                   self.dx, self.dy, self.dt, \
+                                   self.g, self.f, self.r) #, boundary_conditions=self.boundaryConditions)
+
+        t = self.sim.step(self.T)
+        eta1, u1, v1 = self.sim.download()
+        eta2, u2, v2 = loadResults("CDKLM16", "wallBC", "central", "bathymetry_")
+
+        self.checkResults(eta1, u1, v1, eta2, u2, v2)
+       
