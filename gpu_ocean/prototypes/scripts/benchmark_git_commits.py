af864c0332e2cc4c12e877936279e457c9add714
André R. Brodtkorb <Andre.Brodtkorb@sintef.no>
2018-10-11 09:26:25 +0200

    Rewrote script for benchmarking git commits

--- gpu_ocean/prototypes/scripts/benchmark_git_commits.py
+++ gpu_ocean/prototypes/scripts/benchmark_git_commits.py
@@ -26,6 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
 import numpy as np
+import pandas as pd
 import subprocess
 import os, stat
 import sys
@@ -35,192 +36,163 @@ import tempfile
 import shutil
 import logging
 import urllib
-import enum
-
-#current_dir = os.path.dirname(os.path.realpath(__file__))
-#sys.path.insert(0, os.path.abspath(os.path.join(current_dir, '../../')))
-
-import argparse
-parser = argparse.ArgumentParser(description='Benchmark a simulator across git commits.')
-#parser.add_argument('--nx', type=int, default=1000)
-#parser.add_argument('--ny', type=int, default=1000)
-#parser.add_argument('--block_width', type=int)
-#parser.add_argument('--block_height', type=int)
-#parser.add_argument('--steps_per_download', type=int, default=2000)
-#parser.add_argument('--iterations', type=int, default=1)
-parser.add_argument('--simulator', type=str, required=True)
-#parser.add_argument('--output', type=str, default=None)
-parser.add_argument('--log_file', type=str, default="")
-parser.add_argument('--git_commits', type=str, default=None)
-parser.add_argument('--architecture_type', type=str, default="desktop")
 
 
-args = parser.parse_args()
 
 
-if not args.log_file == "":
-	print("ERROR: I haven't implemented writing log to file yet... Sorry :/")
-	sys.exit(-1)
 
+# Helper function for running git commands
+def safe_call(cmd, **kwargs):
+    if (os.name == 'nt'):
+        shell = True
+    else:
+        shell = False
+    
+    
+    stdout = None
+    try:
+        logger.debug("Safe call: " + str(cmd) + " -- " + str(kwargs))
+        stdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=shell, **kwargs)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        if isinstance(output, bytes):
+            output = output.decode("utf-8")
+        logger.error("Failed, return code " + str(e.returncode) + "\n" + output)
+        
+    if isinstance(stdout, bytes):
+        stdout = stdout.decode("utf-8")
+    return stdout
+    
+    
+    
+    
+    
+    
+    
+
+# Setup logging
 logging.getLogger("").setLevel(logging.DEBUG)
-
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logging.getLogger("").addHandler(ch)
-
 logger = logging.getLogger("main")
-logger.debug("Test")
 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(current_dir, '../../')))
 
-if args.git_commits is not None:
-	print("ERROR: Reading this list from file is still not implemented... Sorry :/")
-	sys.exit(-1)
-else:
-    git_versions = [
-        #(git hash, git commit message, block size laptop, block size desktop, block size supercomputer)
-        ("a126bab97e690b7c642814e3c8b96f9879adf487", "original (per arch optimized block size)", (32,4), (12,12), (16, 16), (12, 12)), #Change blocksize here!
-        ("5d817bb7cd2b369039117d19aae7d669a9a2e53a", "Optimized shared memory use Hm", (32,4), (12,12), (16, 16), (12, 12)),
-        ("2e5da01457874ad5087398da77b9102ee991fb94", "Optimized shared memory use RHx/RHy", (32,4), (12,12), (16, 16), (12, 12)),
-        ("fcd145c8c29f3d38a024685fdd0fc3cf9580366c", "Optimized shared memory use Q", (32,4), (12,12), (16, 16), (12, 12)),
-        ("74e957bb41e391a5c5fbb19c3cac393079272dd3", "Optimized shared memory use F", (32,4), (12,12), (16, 16), (12, 12)),
-        ("5aba525d1d64937c03e8d2b33bb7f6d80b97a81a", "Optimized shared memory use G", (32,4), (12,12), (16, 16), (12, 12)),
-        ("addb061fe1cfccebb6fab70f9826be3752162b0b", "Optimized shared memory use Qy", (32,4), (12,12), (16, 16), (12, 12)),
-        ("aab115045fd6d313940fa37be3149155ba4ead70", "Made variables const", (32,4), (12,12), (16, 16), (12, 12)),
-        ("f6a911368b429df608eb5e1218c28bc14d6ffbe2", "Updated blocksize experiment code", (32,4), (12,12), (16, 16), (12, 12)),
-        ("339eb0c9a3bf04875507fab24d8924e24718a2eb", "Changed default block size", (16,8), (24,8), (16, 16), (32, 12)), #Change blocksize here!
-        ("1319edf92c20b8d5fd9fc8c0e0ed58176f9cc2ba", "Tried to reduce register use", (16,8), (24,8), (16, 16), (32, 12)),
-        ("6de871265b820aecc63d68a2e1f858605eb367c7", "Refactored to optimize register use", (16,8), (24,8), (16, 16), (32, 12)),
-        ("f6d1727d747b486203df91079b83a9e717668852", "Added logger to CDKLM", (16,8), (24,8), (16, 16), (32, 12)),
-        ("40f9d4817fa084de7cebfd3cf6d20254b93e7393", "Added fast math compilation flag", (16,8), (24,8), (16, 16), (32, 12)),
-        #("e2af2159becbd3c8769903eb4bea602a96d6c3a1", "Compilation flags experimentation", (16,8), (24,8), (16, 16), (32, 12)),
-        ("12536844bdc4459dcf4cc92776faea3a81d0a32c", "New optimal block size", (32,8), (32,12), (16, 16), (32, 12)) #Change blocksize here!
-    ]
+logger.info("Added " + current_dir + " to path")
 
 
 
-class Architecture(enum.Enum):
-    LAPTOP = 1
-    DESKTOP = 2
-    SUPERCOMPUTER_MET = 3
-    SUPERCOMPUTER_UIO = 4
-	
-architecture = None
-try:
-	architecture = Architecture[args.architecture_type.upper()]
-except KeyError:
-	print("ERROR: architecture_type must be one of the following: [laptop, desktop, supercomputer_met, supercomputer_uio]")
-	sys.exit(-1)
-
-if not (args.simulator == "CDKLM" or args.simulator == "FBL" or args.simulator == "CTCS"):
-	print("ERROR: Unknown simulator type '" + args.simulator + "', aborting")
-	sys.exit(-1)
-logger.info("Using simulator: " + args.simulator)
 
 
-git_url = None
-git_command = "git"
-my_env = os.environ
-shell = True
+#Parse arguments
+import argparse
+parser = argparse.ArgumentParser(description='Benchmark a simulator across git commits.')
+parser.add_argument('--run_benchmark_opts', type=str, default="--simulator CDKLM --steps_per_download 10 --iterations 3")
+parser.add_argument('csv_file', default=None, help="CSV file with columns git_commit, label, block_width, block_height")
+parser.add_argument('--add_exe_path', action='append', type=str, default=[])
+args = parser.parse_args()
+logger.info(args)
 
-# If we are on windows, we might need to modify the path and git command:
-if os.name == 'nt':
-    my_env["PATH"] = "C:\\Program Files\\Git\\mingw64\\bin\\;C:\\Program Files\\Git\\usr\\bin\\;" + my_env["PATH"]
+
+
+
+
+#Read CSV file
+df = pd.read_csv(args.csv_file, comment='#')
+logger.info(df)
+
+
+
+
+#Set git options
+if (os.name == 'nt'):
     git_command = "git.exe"
-if os.name == 'posix':
-    shell = False
-
-try:
-    git_url = subprocess.check_output([git_command, "rev-parse", "--show-toplevel"], stderr=subprocess.STDOUT, shell=shell)
-    git_url = os.path.abspath(os.path.join(git_url.decode("utf-8").rstrip(), ".git"))
-except subprocess.CalledProcessError as e:
-    logger.error("Failed, return code " + str(e.returncode) + "\n" + str(e.output))
+else:
+    git_command = "git"
+   
+# Create modified environment for git to execute in
+my_env = None
+if (len(args.add_exe_path) > 0):
+    my_env = os.environ
+for path in args.add_exe_path:
+    my_env["PATH"] = my_env["PATH"] + ";" + path
+    
+    
+    
+    
+#Get URL of repository
+git_url = safe_call([git_command, "rev-parse", "--show-toplevel"], cwd=current_dir, env=my_env)
+git_url = git_url.strip()
 logger.info(git_url)
-    
-    
-benchmark_script_relpath = "gpu_ocean/demos/testCasesDemos/run_benchmark.py"
-cdklm_python_class_relpath = "gpu_ocean/SWESimulators/CDKLM16.py"
-benchmark_script_version = "5c2214573269367fbdb6d67ace07f2ffa57d37ba"
-benchmark_script_options = ["--simulator", "CDKLM", '--steps_per_download', '10', '--iterations', '3']
-outfile = os.path.join(os.getcwd(), "output_" + time.strftime("%Y_%m_%d-%H_%M_%S") + ".npz")
 
-for version, log, laptop, desktop, supercomputer_met, supercomputer_uio in git_versions:
-    logger.info(version + " - " + log)
-
-
-#Create temporary directory
+#Create temporary directory to clone into
 tmpdir = tempfile.mkdtemp(prefix='git_tmp')
 logger.debug("Writing to " + tmpdir)
 
-# Function for running git commands
-def git(cwd, options):
-    a = None
-    try:
-        cmd = [git_command] + options
-        logger.debug("Git cloning: " + cwd + " -- " + str(cmd))
-        a = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=shell, cwd=cwd, env=my_env)
-    except subprocess.CalledProcessError as e:
-        logger.error("Failed, return code " + str(e.returncode) + "\n" + str(e.output))
-    return a.decode("utf-8") 
-
 # Clone this git repo to tmp    
 git_clone = os.path.join(tmpdir, "git_clone")
-stdout = git(tmpdir, ["clone", git_url, git_clone])
+stdout = safe_call([git_command, "clone", git_url, git_clone], cwd=tmpdir, env=my_env)
 logger.debug(stdout)
 
 
 
+
+
+#Set options for benchmark script
+benchmark_script_relpath = os.path.normpath("gpu_ocean/demos/testCasesDemos/run_benchmark.py")
+benchmark_script_abspath = os.path.join(git_clone, benchmark_script_relpath)
+benchmark_script_version = "90fabc6528a426926069019560fa84e7592d3138"
+outfile = os.path.join(os.getcwd(), "output_" + time.strftime("%Y_%m_%d-%H_%M_%S") + ".npz")
+
+
+
+
+    
+
+
+
+
 # Loop through the git_versions and run each benchmark
-for version, log, laptop_block, desktop_block, supercomputer_met_block, supercomputer_uio_block in git_versions:
-    logger.debug("checkout " + version)
-    logger.debug("    with commit msg: " + log)
-    stdout = git(git_clone, ["checkout", "--force", '-q', version])
-    logger.debug("stdout: " + str(stdout))
+for index, row in df.iterrows():
+    logger.debug("= Start new benchmark ==========================================")
+    print(index)
+    print(row)
+    print(row['git_commit'])
+    logger.debug(str(index) + ": " + str(row['label']))
+    stdout = safe_call([git_command, "checkout", "--force", '-q', row['git_commit']], cwd=git_clone, env=my_env)
+    logger.debug("stdout: \n" + str(stdout))
     
     logger.debug("Checkout " + benchmark_script_relpath)
-    stdout = git(git_clone, ["checkout", "--force", '-q', benchmark_script_version, "--", benchmark_script_relpath])
-    logger.debug("stdout: " + str(stdout))
+    stdout = safe_call([git_command, "checkout", "--force", '-q', benchmark_script_version, "--", benchmark_script_relpath], cwd=git_clone, env=my_env)
+    logger.debug("stdout: \n" + str(stdout))
         
-    block_size_options = None
-
-    if architecture == Architecture.DESKTOP or architecture == Architecture.SUPERCOMPUTER_MET or architecture == Architecture.SUPERCOMPUTER_UIO:
-        
-        if architecture == Architecture.LAPTOP:
-            block_size_options = ["--block_width", str(laptop_block[0]), "--block_height", str(laptop_block[1])] 
-
-            
-        if architecture == Architecture.DESKTOP:
-            block_size_options = ["--block_width", str(desktop_block[0]), "--block_height", str(desktop_block[1])]
-            
-        if architecture == Architecture.SUPERCOMPUTER_MET:
-            block_size_options = ["--block_width", str(supercomputer_met_block[0]), "--block_height", str(supercomputer_met_block[1])]
-            
-        if architecture == Architecture.SUPERCOMPUTER_UIO:
-            block_size_options = ["--block_width", str(supercomputer_uio_block[0]), "--block_height", str(supercomputer_uio_block[1])]
-                   
-    a = None
-    try:
-        #cmd = ["python", "--version"]
-        cmd = ["python", os.path.join(git_clone, benchmark_script_relpath)] + benchmark_script_options + ["--output", "benchmark_" + version + ".npz"] + block_size_options
-        a = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=shell, cwd=tmpdir, env=my_env)
-    except subprocess.CalledProcessError as e:
-        logger.error("Failed, return code " + str(e.returncode) + "\n" + str(e.output))
-    logger.debug("Output:\n" + a.decode("utf-8"))
+    options = args.run_benchmark_opts.split(' ')
+    options += ["--output", "benchmark_" + row['git_commit'] + ".npz"] 
+    options += ["--block_width", str(row['block_width']), "--block_height", str(row['block_height'])]
+    cmd = ["python", benchmark_script_abspath] + options
+    stdout = safe_call(cmd, cwd=tmpdir, env=my_env)
+    if isinstance(stdout, bytes):
+        stdout = stdout.decode("utf-8")
+    logger.debug("stdout:\n" + stdout)
     
     # Cleaning git folder so that it can be deleted
     #stdout = git(git_clone, ["-n", ])
         
-print("benchmarking finished")
+logger.debug("*************** benchmarking finished **********************")
 
 
 
-versions, labels, laptop_block, desktop_block, supercomputer_met_block, supercomputer_uio_block = list(zip(*git_versions))
-megacells = np.full((len(versions)), np.nan)
 
-for i, ver in enumerate(git_versions):
-    version, log, l_block, d_block, s_met_block, s_uio_block = ver
-    filename = os.path.join(tmpdir, "benchmark_" + version + ".npz")
+
+#Save results to file
+megacells = np.full((len(df.index)), np.nan)
+
+for index, row in df.iterrows():
+    filename = os.path.join(tmpdir, "benchmark_" + row['git_commit'] + ".npz")
     with np.load(filename) as version_data:
-        megacells[i] = version_data['CDKLM']
+        megacells[i] = version_data['megacells']
 
-np.savez(outfile, versions=versions, labels=labels, megacells=megacells)
+np.savez(outfile, versions=df['git_commit'], labels=df['label'], megacells=megacells)
 
