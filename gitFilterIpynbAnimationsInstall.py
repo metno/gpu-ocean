4e140144e073d808f3e299816b34460347b01ac2
Florian Beiser <florian.beiser@sintef.no>
2020-12-02 11:15:35 +0100

    filter animations

---
+++ gitFilterIpynbAnimationsInstall.py
@@ -0,0 +1,31 @@
+"""
+gitFilterIpynbAnimationsInstall.py
+
+Tailormade for the GPU-Ocean repository
+
+Inspired by https://github.com/kynan/nbstripout
+
+
+Instructions:
+
+1.  cloning the gpuocean repository
+2.  follow the instructions to set the conda environment
+3.  run this script by `ipython gitFilterIpynbAnimationsInstall.py' in the gpuocean folder!
+
+Effect:
+A git filter will be set in the config file which prevents to commit animations in notebooks 
+and helps to keep the worktree cleaner
+
+"""
+
+import sys
+
+with open('.git/config', 'a') as f:
+	f.write('\n')
+	f.write('[filter "ipynbAnimations"]\n')
+	filepath = '"{}" gitFilterIpynbAnimations.py'.format(sys.executable.replace('\\', '/')).replace('exe','exe\\')
+	f.write('   clean = \\' + filepath + '\n')
+	f.write('   smudge = cat')
+   
+with open('.gitattributes', 'a', newline='') as f:
+    f.write('*.ipynb filter=ipynbAnimations\n')
