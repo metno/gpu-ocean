7e287265ace5c69fd7870cdcc063e9dc2081f797
FlorianBeiser <florian.beiser@sintef.no>
2022-03-02 14:08:18 +0100

    run time comparisons

---
+++ gpu_ocean/prototypes/ETKF/computational_performance.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+"""
+This software is part of GPU Ocean. 
+
+Copyright (C) 2019 SINTEF Digital
+
+This python program is used to set up and run a data-assimilation 
+and drift trajectory forecasting experiment.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+import sys, os, json, datetime, time, shutil
+import numpy as np
+
+current_dir = os.getcwd()
+
+if os.path.isdir(os.path.abspath(os.path.join(current_dir, '../../SWESimulators'))):
+        sys.path.insert(0, os.path.abspath(os.path.join(current_dir, '../../')))
+
+# Knonw hosts: havvarsel, PPI.
+host = os.uname()[1] 
+if host in ["r740-5hdn2s2-ag-gcompute", "r740-5hcv2s2-ag-gcompute", "r740-dsxm2t2-ag-gcompute", "r740-dsws2t2-ag-gcompute"]:
+    host = "ppi"
+
+
+if host == "ppi":
+    media_dir='/lustre/storeB/users/florianb/forecasting_results/'
+elif host == "havvarsel":
+    media_dir='/sintef/forecasting_results/'
+else:
+    print("Unknown host")
+    sys.exit(0)
+
+
+###-----------------------------------------
+## Define files for ensemble and truth.
+##
+
+if host == "ppi":
+    ensemble_init_path = '/lustre/storeB/users/florianb/data/ensemble_init/'
+elif host == "havvarsel":
+    ensemble_init_path = '/sintef/data/ensemble_init/'
+else:
+    ensemble_init_path = 'Give path here'
+assert len(os.listdir(ensemble_init_path)) == 100 or len(os.listdir(ensemble_init_path)) == 101,\
+    "Ensemble init folder has wrong number of files: " + str(len(os.listdir(ensemble_init_path)))
+
+if host == "ppi":
+    truth_path = '/lustre/storeB/users/florianb/data/true_state/'
+elif host == "havvarsel":
+    truth_path = '/sintef/data/true_state/'
+else:
+    truth_path = 'Give path here'
+assert len(os.listdir(truth_path)) == 2 or len(os.listdir(truth_path)) == 3,\
+    "Truth folder has wrong number of files"
+
+
+timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+destination_dir = os.path.join(media_dir, "performance_experiment_" +  timestamp + "/")
+os.makedirs(destination_dir)
+
+
+###--------------------------------
+# Import required packages
+#
+# For GPU contex:
+from SWESimulators import Common
+# For the ensemble:
+from SWESimulators import EnsembleFromFiles, Observation
+# For data assimilation:
+from SWESimulators import IEWPFOcean
+# The prototyping packages:
+#import EnKFOcean #(different prototype folder)
+import ETKFOcean
+import SkillScore
+# For ObservationType:
+from SWESimulators import DataAssimilationUtils as dautils
+
+# Create CUDA context
+gpu_ctx = Common.CUDAContext()
+device_name = gpu_ctx.cuda_device.name()
+
+#----------------------------------------------------
+# Performance set-up
+
+methods = ["MC", "IEWPF2", "LETKF"]
+ensemble_sizes = [50, 100, 250]
+
+performance = np.zeros((3,3,72))
+
+for m in range(len(methods)):
+    method = methods[m]
+    for e in range(len(ensemble_sizes)):
+        ###--------------------------
+        # Initiate the ensemble
+
+        ensemble = EnsembleFromFiles.EnsembleFromFiles(gpu_ctx, 
+                                                        ensemble_sizes[e],                                                
+                                                        ensemble_init_path, 
+                                                        truth_path,                                                
+                                                        1.0,
+                                                        cont_write_netcdf = False,
+                                                        use_lcg = False, xorwow_seed = np.random.randint(1,10000),
+                                                        write_netcdf_directory = destination_dir,
+                                                        observation_type=dautils.ObservationType.StaticBuoys)
+
+        # Configure observations according to the selected drifters:
+        ensemble.configureObservations(drifterSet=[], 
+                                    observationInterval = 1,
+                                    buoy_area = "all")
+
+
+        ### -------------------------------
+        # Initialize DA class (if needed)
+        #
+        if method.startswith('iewpf'):
+            iewpf = IEWPFOcean.IEWPFOcean(ensemble)
+        elif method.startswith('etkf') or method.startswith('letkf'):
+            etkf = ETKFOcean.ETKFOcean(ensemble)
+
+        ### ----------------------------------------------
+        #   DATA ASSIMILATION
+        #
+        h = 0
+
+        obstime = 3*24*60*60 # time in seconds (starting after spin-up phase)
+        for day in range(3):
+            
+            tic = time.time()
+            
+            for hour in range(24):
+                
+                for fiveMin in range(12):
+                    
+                    for minute in range(5):
+                        obstime += 60
+                        if method == 'iewpf2':
+                            ensemble.stepToObservation(obstime, model_error_final_step=(minute<4))
+                            if minute == 4:
+                                iewpf.iewpf_2stage(ensemble, perform_step=False)
+                        else:
+                            ensemble.stepToObservation(obstime)
+                            if minute == 4:
+                                if method == 'etkf':
+                                    etkf.ETKF(ensemble)
+                                if method == 'letkf':
+                                    etkf.LETKF(ensemble)
+                    # Done minute
+                # Done five minutes
+            # Done hour
+            toc = time.time()
+            performance[m,e,h] = toc - tic
+            h = h+1 
+                
+
+        # Clean up simulation and close netcdf file
+        ensemble.cleanUp()
+
+
+
+
