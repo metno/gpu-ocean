68250cb783c276db74a0269059f35b34d0da3edd
FlorianBeiser <florian.beiser@sintef.no>
2022-03-17 14:51:33 +0100

    split DA paper folders

---
+++ gpu_ocean/papers/DAComparison/scripts/run_rank_hist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+output_dir="tmp/rank_hist_iewpf_$(date +%Y_%m_%d-%H_%M_%S)"
+echo $output_dir
+
+which python
+
+mkdir $output_dir
+
+python rank_histogram_experiment.py --method letkf --experiment 1 --output_folder $output_dir
+
+python rank_histogram_experiment.py --method iewpf2 --experiment 1 --output_folder $output_dir
+
+scp -r $output_dir ubuntu@gpu-ocean.met.no:
